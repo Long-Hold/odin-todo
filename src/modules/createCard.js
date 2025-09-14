6bde3d6742d4fb27518ace2ba84a70f74e47bba2
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-14 18:28:31 -0400

    refactor: Iterate over fields in formData and dynamically add to todo card class

--- src/modules/createCard.js
+++ src/modules/createCard.js
@@ -1,16 +1,13 @@
 class TodoCard {
-    #title;
-    #priority;
-
-    constructor(title, priority) {
-        this.#title = title;
-        this.#priority = priority;
-    }
-
-    get title() { return this.#title; }
-    get priority() { return this.#priority; }
 }
 
 export function createTodoCard(formData) {
-    return new TodoCard(formData.get('title'), formData.get('priority'));
+    const card = new TodoCard();
+    for (const [key, value] of formData.entries()) {
+        if (value) {
+            card[key] = value;
+        }
+    }
+
+    return card;
 }
