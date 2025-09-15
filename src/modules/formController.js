66cb96e15b2a8c079403cf97ae4ab56e0d476ac3
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-15 13:02:25 -0400

    feat: Add private module to manipulate the form

--- src/modules/formController.js
+++ src/modules/formController.js
@@ -1,3 +1,28 @@
+const formManipulator = (function() {
+    const todoStepsContainer = document.getElementById('checklist-container');
+    let stepCounter = 1;
+
+    todoStepsContainer.addEventListener('click', (event) => {
+        if (event.target.type === 'button') {
+            createChecklistNode();
+        }
+    })
+
+    const createChecklistNode = () => {
+        const inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.name = `step${stepCounter}`;
+
+        const label = document.createElement('label');
+        label.textContent = `Step ${stepCounter}`;
+
+        label.appendChild(inputField);
+        ++stepCounter;
+
+        todoStepsContainer.appendChild(label);
+    }
+})();
+
 export function formController() {
     const formNode = document.getElementById('new-task');
     const dialog = document.querySelector('dialog');
