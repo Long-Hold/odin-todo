feea06ae2239eae359317a0d20c158fd737ee30c
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-16 13:38:34 -0400

    feat: Add private method to delegate events, add basic submit handler

--- src/modules/formController.js
+++ src/modules/formController.js
@@ -1,69 +1,83 @@
-class formControl {
+export class formControl {
+    static #classInvoked = false;
     static #formNode = document.getElementById('new-todo-form');
     static #dialog = document.querySelector('dialog');
     static #checkListContainer = document.getElementById('checklist-container');
 
-    static #dialogController() {
-        formControl.#dialog.addEventListener('click', (event) => {
-            if (event.target.type === 'button' && event.target.textContent === 'Cancel') {
-                formControl.#dialog.close();
-            }
+    static #processSubmit(event) {
+        const newTodoData = new FormData(event.target);
+        const formSubmitted = new CustomEvent('todoSubmitted', { detail: {newTodoData} });
+        document.dispatchEvent(formSubmitted);
+    }
+
+    static #eventDelegator() {
+        formControl.#formNode.addEventListener('submit', (event) => {
+            event.preventDefault();
+            formControl.#processSubmit(event);
+            formControl.#formNode.reset();
+            formControl.#dialog.close();
         })
     }
 
     static initializeEventListeners() {
-        formControl.#dialogController();
+        if (formControl.#classInvoked) {
+            console.error('formControl class already invoked');
+            return;
+        }
+
+        formControl.#classInvoked = true;
+        formControl.#eventDelegator();
     }
 }
 
-function addChecklistStep(containerNode, stepCounter) {
-        const inputField = document.createElement('input');
-        inputField.type = 'text';
-        inputField.name = `step${stepCounter}`;
-        inputField.maxLength = 64;
+// function addChecklistStep(containerNode, stepCounter) {
+//         const inputField = document.createElement('input');
+//         inputField.type = 'text';
+//         inputField.name = `step${stepCounter}`;
+//         inputField.maxLength = 64;
 
-        const label = document.createElement('label');
-        label.textContent = `Step ${stepCounter}: `;
+//         const label = document.createElement('label');
+//         label.textContent = `Step ${stepCounter}: `;
 
-        label.appendChild(inputField);
+//         label.appendChild(inputField);
 
-        const stepDivContainer = document.createElement('div');
-        stepDivContainer.appendChild(label);
+//         const stepDivContainer = document.createElement('div');
+//         stepDivContainer.appendChild(label);
 
-        containerNode.querySelector('section').appendChild(stepDivContainer);
-}
+//         containerNode.querySelector('section').appendChild(stepDivContainer);
+// }
 
-export function formController() {
-    const headerbtn = document.getElementById('add-task');
-    const formNode = document.getElementById('new-todo-form');
-    const dialog = document.querySelector('dialog');
-    const checkListContainer = document.getElementById('checklist-container');
-    let stepCounter = 1;
+// export function formController() {
+//     const headerbtn = document.getElementById('add-task');
+//     const formNode = document.getElementById('new-todo-form');
+//     const dialog = document.querySelector('dialog');
+//     const checkListContainer = document.getElementById('checklist-container');
+//     let stepCounter = 1;
 
-    headerbtn.addEventListener('click', () => {
-        checkListContainer.querySelector('section').innerHTML = '';
-        dialog.showModal()
-    });
+//     headerbtn.addEventListener('click', () => {
+//         checkListContainer.querySelector('section').innerHTML = '';
+//         dialog.showModal()
+//     });
 
-    checkListContainer.addEventListener('click', (event) => {
-        if (event.target.type === 'button') {
-            addChecklistStep(checkListContainer, stepCounter);
-            ++stepCounter;
-        }
-    })
+//     checkListContainer.addEventListener('click', (event) => {
+//         if (event.target.type === 'button') {
+//             addChecklistStep(checkListContainer, stepCounter);
+//             ++stepCounter;
+//         }
+//     })
 
-    formNode.addEventListener('submit', (event) => {
-        event.preventDefault();
-        stepCounter = 1;
+//     formNode.addEventListener('submit', (event) => {
+//         event.preventDefault();
+//         stepCounter = 1;
 
-        const formData = new FormData(event.target);
-        formNode.reset();
-        dialog.close();
+//         const formData = new FormData(event.target);
+//         formNode.reset();
+//         dialog.close();
 
-        const formSubmitted = new CustomEvent('onSubmit', {
-            detail: { formData }
-        });
+//         const formSubmitted = new CustomEvent('onSubmit', {
+//             detail: { formData }
+//         });
 
-        document.dispatchEvent(formSubmitted);
-    })
-}
+//         document.dispatchEvent(formSubmitted);
+//     })
+// }
