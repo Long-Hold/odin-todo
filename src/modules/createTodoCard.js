b3b024097df2cf280bc11241cd34b76e71106155
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-20 14:33:19 -0400

    refactor: Remove current implementation for total rework

--- src/modules/createTodoCard.js
+++ src/modules/createTodoCard.js
@@ -5,22 +5,4 @@
 
 export const createTodoCard = () => {
     const todoCard = {};
-
-    const init = () => {
-        const cardTemplate = document.getElementById('todo-card-template');
-        todoCard.container = cardTemplate.content.cloneNode(true);
-
-        todoCard.status = todoCard.container.querySelector('.todo-status')
-        todoCard.title = todoCard.container.querySelector('.todo-title');
-        todoCard.category = todoCard.container.querySelector('.project-category');
-        todoCard.dueDate = todoCard.container.querySelector('time');
-
-        todoCard.description = todoCard.container.querySelector('.description');
-
-        todoCard.description = todoCard.container.querySelector('.todo-checklist');
-    }
-
-    const getTodoCard = () => { return todoCard.status; }
-
-    return {init, getTodoCard};
 }
