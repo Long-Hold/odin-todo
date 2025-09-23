f0cbe8a4b2e86b15aa4e566890516c37112861dc
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-23 18:48:50 -0400

    refactor: Remove old bundlingn logic and attach manager to window

--- src/index.js
+++ src/index.js
@@ -7,24 +7,6 @@ FormControl.initializeEventListeners();
 
 document.addEventListener('todoSubmitted', (event) => {
     const { formData } = event.detail;
-
-    const processedData = new FormData();
-    const steps = {};
-
-    for (const [key, value] of formData.entries()) {
-        if (key.startsWith('step')) {
-            steps[key] = value;
-        }
-
-        else {
-            processedData.append(key, value);
-        }
-    }
-
-    if (Object.keys(steps).length > 0) {
-        processedData.append('steps', JSON.stringify(steps));
-    }
-
     const newCard = createTodoObject(formData);
     todoObjManager.addTodoObj(newCard);
 })
@@ -43,4 +25,5 @@ const todoObjManager = (function() {
     return {addTodoObj, getAllTodoObjs, getTodoObj, deleteTodoObj};
 })();
 
+window.todoObjManager = todoObjManager;
 window.cardCreator = cardCreator();
