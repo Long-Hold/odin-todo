ecce0d06d30de7042fe161a61286ef9cd68b315b
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-23 11:46:29 -0400

    refactor: Use module to create todo cards instead of classes

--- src/modules/createTodoCard.js
+++ src/modules/createTodoCard.js
@@ -1,74 +1,25 @@
 import { format, comapreAsc, isMatch, isValid, set } from "date-fns"
 import { de } from "date-fns/locale";
 
-export class TodoCard {
-    static #validPriorities = ['low','medium','high'];
+const cardCreator = (function() {
+    const template = document.getElementById('todo-card-template');
+    let todoCard;
 
-    #template = document.getElementById('todo-card-template').content.cloneNode(true);
+    const initializeTemplate = () => { todoCard = template.content.cloneNode(true); }
 
-    #title = this.#template.querySelector('.todo-title');
-    #priority = this.#template.querySelector('.priority');
-    #project = this.#template.querySelector('.project-category');
-    #deadline = this.#template.querySelector('time');
-    #description = this.#template.querySelector('.description');
-    #checklistSteps = this.#template.querySelector('.todo-checklist');
-    #taskID = this.#template.querySelector('.todo-card');
-
-    constructor(taskID) {
-        this.#taskID.dataset.taskid = taskID;
-    }
-
-    get template() { return this.#template; }
-
-    get title() { return this.#title; }
-
-    get priority() { return this.#priority; }
-
-    get project() { return this.#project; }
-
-    get deadline() { return this.#deadline; }
-
-    get description() { return this.#description; }
-
-    get checklistSteps() { return this.#checklistSteps; }
-
-    get taskID() { return this.#taskID.dataset.taskid; }
-
-    set title(title) {
+    const setTitle = (title) => {
         if (typeof(title) !== 'string') {
-            throw new TypeError('Title must be a string');
+            throw new TypeError('Title must be of type: String');
         }
-
         if (title.trim().length === 0) {
-            throw new Error('Title cannot be empty or contain only whitespace');
+            throw new Error('Title cannot be empty');
         }
-
+        
         if (title.length > 64) {
-            throw new RangeError('Title cannot exceed 64 characters');
+            throw new Error('Title cannot be longer than 64 characters');
         }
-
-        this.#title.textContent = title.trim();
+        todoCard.querySelector('todo-title').textContent = title; 
     }
+})
 
-    set priority(priority) {
-        if (TodoCard.#validPriorities.includes(priority.toLowerCase())) {
-            this.#priority.textContent = priority;
-        }
-
-        else {
-            throw new Error('Invalid priority passed to setter');
-        }
-    }
-
-    set project(project) {
-        this.#project = project;
-    }
-
-    set deadline(deadline) {
-        if (!isMatch(deadline, 'yyyy-mm-dd')){
-            throw new TypeError('Deadline format must be yyyy-mm-dd');
-        }
-
-        this.#deadline = deadline;
-    }
-}
+export {cardCreator};
