c353add26ef8f7ba241ec1da23e4b6519786519f
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-23 12:05:56 -0400

    feat: Add method to set priority

--- src/modules/createTodoCard.js
+++ src/modules/createTodoCard.js
@@ -3,6 +3,7 @@ import { de } from "date-fns/locale";
 
 const cardCreator = (function() {
     const template = document.getElementById('todo-card-template');
+    const validPriorities = ['Low','Medium','High'];
     let todoCard;
 
     const initializeTemplate = () => { todoCard = template.content.cloneNode(true); }
@@ -19,11 +20,23 @@ const cardCreator = (function() {
         if (title.length > 64) {
             throw new Error('Title cannot be longer than 64 characters');
         }
-        
+
         todoCard.querySelector('.todo-title').textContent = title; 
     }
 
-    return {initializeTemplate, setTitle};
+    const setPriority = (priority) => {
+        const text = priority.trim();
+        if (text.length === 0) {
+            throw new Error('Priority field cannot be blank');
+        }
+
+        if (!validPriorities.includes(text)) {
+            throw new TypeError('Invalid string passed as priority');
+        }
+
+        todoCard.querySelector('.priority').textContent = text;
+    }
+    return {initializeTemplate, setTitle, setPriority};
 })
 
 export {cardCreator};
