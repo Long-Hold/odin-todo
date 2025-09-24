641f04062719786ba40e3ba9fbdc86b7415999af
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-24 13:46:12 -0400

    feat: Attach createCardCreator to window

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles/styles.css";
 import { createTodoObject } from "./modules/createTodoObj";
 import { FormControl } from "./modules/formController";
-import { cardCreator } from "./modules/createTodoCard";
+import { cardCreator, createCardCreator } from "./modules/createTodoCard";
 
 FormControl.initializeEventListeners();
 
@@ -26,4 +26,4 @@ const todoObjManager = (function() {
 })();
 
 window.todoObjManager = todoObjManager;
-window.cardCreator = cardCreator();
+window.cardCreator = createCardCreator();
