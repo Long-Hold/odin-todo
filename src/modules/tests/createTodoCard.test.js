bddea5f6d8e76b3c2c4ab8c3cd2932792a08ec99
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-09-28 17:14:29 -0400

    refactor: Group tests into describe blocks

--- src/modules/tests/createTodoCard.test.js
+++ src/modules/tests/createTodoCard.test.js
@@ -39,38 +39,58 @@ beforeEach(() => {
     `;
 });
 
+describe('createCardCreator', () => {
+    let cardCreator;
+    beforeEach(() => {
+        document.body.innerHTML = `
+            <template id="todo-card-template">
+                <article class="todo-card" data-taskID="">
+                    <header>
+                        <button type="radio" class="todo-status"></button>
+                        <h3 class="todo-title">Todo Title</h3>
+                        <p class="priority"></p>
+                        <p class="project-category"></p>
+                        <time datetime=""></time>
+                    </header>
 
-test('setCardID rejects non-string parameter', () => {
-    const cardCreator = createCardCreator();
-    expect(() => cardCreator.setCardID(1)).toThrow(TypeError);
-    expect(() => cardCreator.setCardID()).toThrow(TypeError);
-    expect(() => cardCreator.setCardID({})).toThrow(TypeError);
-});
+                    <div class="todo-desc">
+                        <h4>DESCRIPTION</h4>
+                        <p class="description"></p>
+                    </div>
 
-test('setCardID rejects empty string parameter', () => {
-    const cardCreator = createCardCreator();
-    expect(() => cardCreator.setCardID('')).toThrow(Error);
-    expect(() => cardCreator.setCardID('          ')).toThrow(Error);
-    expect(() => cardCreator.setCardID(`${' '}`)).toThrow(Error);
-});
+                    <div class="todo-checklist">
+                        <fieldset>
+                            <legend>To Complete</legend>
+                            <div class="checklist-container">
+                            </div>
+                        </fieldset>
+                    </div>
 
-test('setTitle rejects non-string parameter', () => {
-    const cardCreator = createCardCreator();
-    expect(() => cardCreator.setTitle(123)).toThrow(TypeError);
-    expect(() => cardCreator.setTitle([]).toThrow(TypeError));
-    expect(() => cardCreator.setTitle({hey: 'hey'}).toThrow(TypeError));
-    expect(() => cardCreator.setTitle(new String()).toThrow(TypeError));
-})
+                    <div class="todo-notes">
+                        <h4>NOTES</h4>
+                        <p></p>
+                    </div>
 
-test('setTItle reject empty string parameter', () => {
-    const cardCreator = createCardCreator();
-    expect(() => cardCreator.setTitle('')).toThrow(Error);
-    expect(() => cardCreator.setTitle('     ')).toThrow(Error);
-    expect(() => cardCreator.setTitle(` `)).toThrow(Error);
-})
+                    <div class="modify-btns">
+                        <button type="button" class="delete-btn">Delete</button>
+                        <button type="button" class="edit-btn">Edit</button>
+                    </div>
+                </article>
+            </template>
+        `;
 
-test('setTitle rejects strings that exceed max length', () => {
-    const cardCreator = createCardCreator();
-    expect(() => cardCreator.setTitle('A'.repeat(65))).toThrow(Error);
-    expect(() => cardCreator.setTitle('A'.repeat(80))).toThrow(Error);
+        cardCreator = createCardCreator();
+    });
+
+    describe('setCardID', () => {
+        describe('when given invalid input', () => {
+            test.each([
+                [1, 'number'],
+                [undefined, 'undefined'],
+                [{}, 'object']
+            ])('throws TypeError for %s input', (input) => {
+                expect(() => cardCreator.setCardID(input)).toThrow(TypeError);
+            })
+        })
+    })
 })
