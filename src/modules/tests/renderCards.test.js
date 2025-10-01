e4cab6bb0fa711e06522bb46d4de40ce40c20e8d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-01 19:36:53 -0400

    feat: Add tests to validate typeerror conditions

--- src/modules/tests/renderCards.test.js
+++ src/modules/tests/renderCards.test.js
@@ -6,5 +6,18 @@ describe('displayNewCardNode', () => {
             <section class="display-box">
             </section>
         `;
-    })
+    });
+
+    describe('when passed invalid input', () => {
+        test.each([
+            {description: 'number', input: 1},
+            {description: 'array', input: []},
+            {description: 'object literal', input: {}},
+            {description: 'empty string', input: ''},
+            {description: 'whitespace string', input: '   '},
+            {description: 'string', input: 'Hello, world'}
+        ])('throws TypeError for $description input', ({description, input}) => {
+            expect(() => displayNewCardNode(input)).toThrow(TypeError);
+        });
+    });
 })
