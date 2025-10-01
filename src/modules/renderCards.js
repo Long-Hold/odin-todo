4e014841f1533ff53edeff04a24f3938f2eba5b7
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-09-30 20:17:17 -0400

    refactor: Add error checking to display module function

--- src/modules/renderCards.js
+++ src/modules/renderCards.js
@@ -1,4 +1,12 @@
 export function displayNewCardNode(todoCard) {
+    if (todoCard instanceof Node === false) {
+        throw new TypeError('Incorrect instance passed. Expected type: Node');
+    }
+
+    if (todoCard.classList.contains('todo-card') === false) {
+        throw new Error('Invalid Node Element passed. Expected Class: todo-card');
+    }
+    
     const cardDisplayNode = document.querySelector('.display-box');
     cardDisplayNode.appendChild(todoCard);
 
