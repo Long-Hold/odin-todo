67eaac23d697bf2197e11925c94780c513645355
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-02 22:58:10 -0400

    feat: Add function to call set methods as needed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,46 @@ document.addEventListener('todoSubmitted', (event) => {
     const {data} = event.detail;
     const newCard = createTodoObject(data);
     todoObjManager.addTodoObj(newCard);
+
+    const cardNode = customizeTodoCard(newCard);
+    displayNewCardNode(cardNode);
 })
+
+function customizeTodoCard(todoCardObj) {
+    const cardCreator = createCardCreator();
+    for (const property in todoCardObj) {
+        switch (property) {
+            case ('taskID'):
+                cardCreator.setCardID(todoCardObj[property]);
+                break;
+            
+            case ('title'):
+                cardCreator.setTitle(todoCardObj[property]);
+                break;
+
+            case ('priority'):
+                cardCreator.setPriority(todoCardObj[property]);
+                break;
+            
+            case('projects'):
+                cardCreator.setProject(todoCardObj[property]);
+                break;
+            
+            case('deadline'):
+                cardCreator.setDeadline(new Date(todoCardObj[property]));
+                break;
+            
+            case('description'):
+                cardCreator.setDescription(todoCardObj[property]);
+            
+            case('steps'):
+                cardCreator.setChecklistSteps(todoCardObj[property])
+                break;
+        }
+    }
+
+    return cardCreator.getTodoCard();
+}
 
 const todoObjManager = (function() {
     const activeTodos = {};
