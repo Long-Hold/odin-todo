9617cc439cc733ba2141b2e17e0bd8ac8bb4dc11
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-06 11:32:08 -0400

    feat: Add tests to verify TypeError throwing works

--- src/modules/tests/formController.test.js
+++ src/modules/tests/formController.test.js
@@ -1,1 +1,14 @@
 import { objectifySubmission, bundleKeys } from "../formController";
+
+describe('objectifySubmission', () => {
+    describe('when passed non FormData parameter', () => {
+        test.each([
+            {description: 'number', input: 142},
+            {description: 'array', input: ['array', 'array two']},
+            {description: 'object-literal', input: {date: 'today'}},
+            {description: 'string', input: 'some string'},
+        ])('throws TypeError for $description input', ({description, input}) => {
+            expect(() => objectifySubmission(input)).toThrow(TypeError);
+        })
+    })
+})
