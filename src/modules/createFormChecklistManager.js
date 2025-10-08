b4aee3149458596fb47971686990e0dfe67462cc
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-08 12:34:26 -0400

    feat: Add parameter and error checking for template

--- src/modules/createFormChecklistManager.js
+++ src/modules/createFormChecklistManager.js
@@ -1,7 +1,12 @@
-export function createChecklistManager(checklistNode) {
+export function createChecklistManager(checklistNode, template) {
     if (checklistNode instanceof HTMLElement === false) {
         throw new TypeError(`Expected HTMLElement. Received ${typeof(checklistNode)}`);
     }
     
+    if (template instanceof HTMLTemplateElement === false) {
+        throw new TypeError(`Expected HTMLTemplateElement. Received ${typeof(template)}`);
+    }
+
     const checklistContainer = checklistNode;
+    const inputTemplate = template;
 }
