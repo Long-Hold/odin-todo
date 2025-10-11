f86a100eee7e6d947864c91f7674363e144b492e
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-11 19:16:45 -0400

    refactor: Make deep copy of new objects before creating nodes from them

--- src/index.js
+++ src/index.js
@@ -45,11 +45,11 @@ function customizeTodoCard(todoCardObj) {
 const todoObjManager = (function() {
     const activeTodos = {};
 
-    const addTodoObj = (todoObj) => activeTodos[todoObj.taskID] = todoObj;
+    const addTodoObj = (todoObj) => { activeTodos[todoObj.taskID] = structuredClone(todoObj); };
     
     const getAllTodoObjs = () => activeTodos;
 
-    const getTodoObj = (taskID) => activeTodos[taskID];
+    const getTodoObj = (taskID) => { return structuredClone(activeTodos[taskID])} ;
 
     const deleteTodoObj = (taskID) => delete activeTodos[taskID];
 
@@ -57,69 +57,6 @@ const todoObjManager = (function() {
 })();
 
 window.todoObjManager = todoObjManager;
-
-function formTransactor() {
-    const newTask = document.getElementById('add-task');
-
-    const dialog = document.querySelector('dialog');
-
-    const form = document.getElementById('new-todo-form');
-
-    const checklistInputContainer = form.querySelector('#input-steps-container');
-    const checklistInputTemplate = document.getElementById('checklist-step-input');
-    const checklistManager = createChecklistManager(checklistInputContainer, checklistInputTemplate);
-
-    newTask.addEventListener('click', () => dialog.show() );
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        try {
-            const cleanedFormData = removeEmptyFields(new FormData(event.target));
-            let formObject = objectifySubmission(cleanedFormData);
-
-            if (Object.keys(formObject).some(key => key.startsWith('step'))) {
-                formObject = bundleKeys(formObject, 'step', 'steps');
-            }
-
-            const todoObject = createTodoObject(formObject);
-            const todoCard = customizeTodoCard(todoObject);
-
-            displayNewCardNode(todoCard);
-            todoObjManager.addTodoObj(todoObject);
-        } catch (error) {
-            console.error(`An Error occured during Form Transaction: ${error}`);
-        } finally {
-            checklistManager.deleteAllInputFields();
-            form.reset();
-            dialog.close();
-        }
-    })
-
-    form.addEventListener('click', (event) => {
-        if (event.target.id === 'add-step') {
-            checklistManager.addInputField();
-        }
-
-        if (event.target.dataset.action === 'delete') {
-            /**If the delete button was selected, we pass the parent div container
-             * that encapsulates the checklist label, input, and button itself to the
-             * deletion function.
-              */
-            checklistManager.deleteInputField(event.target.parentElement);
-            checklistManager.renumberInputFields();
-        }
-
-        if (event.target.type === 'reset') {
-            checklistManager.deleteAllInputFields();
-        }
-
-        if (event.target.dataset.action === 'cancel') {
-            checklistManager.deleteAllInputFields();
-            form.reset();
-            dialog.close();
-        }
-    })
-}
 
 function formEventDelegator() {
     const form = document.getElementById('new-todo-form');
