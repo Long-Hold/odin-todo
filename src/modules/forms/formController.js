546590f1b80cb5e0cfb0636cf8d811dfd69ba57d
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-15 10:02:34 -0400

    feat: Target form related DOM elements

--- src/modules/forms/formController.js
+++ src/modules/forms/formController.js
@@ -1,2 +1,6 @@
 import { objectifySubmission, bundleKeys, removeEmptyFields } from "./formUtils";
 import { createChecklistManager } from "./createFormChecklistManager";
+
+const newTodoForm = document.getElementById('new-todo-form');
+const dialog = document.querySelector('dialog');
+const newTodoBtn = document.getElementById('add-task');
