23f39f3ddd664d9600bd03c81dddb1fd2ae5dc1f
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-15 10:55:55 -0400

    feat: Create function to listen for new task btn press

--- src/modules/forms/formController.js
+++ src/modules/forms/formController.js
@@ -3,4 +3,14 @@ import { createChecklistManager } from "./createFormChecklistManager";
 
 const newTodoForm = document.getElementById('new-todo-form');
 const dialog = document.querySelector('dialog');
-const newTodoBtn = document.getElementById('add-task');
+
+function initializeNewTodoListener() {
+    const newTodoBtn = document.getElementById('add-task');
+    newTodoBtn.addEventListener('click', () => {
+        dialog.show();
+    })
+}
+
+export function initializeFormControl() {
+    initializeNewTodoListener();
+}
