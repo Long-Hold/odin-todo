d6a5b79b7ed2b15454fdcd133626ca4e24af26bb
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-16 16:59:45 -0400

    feat: Create function that adds todo objects to map

---
+++ src/modules/todoObjects/todoManager.js
@@ -0,0 +1,17 @@
+const activeTodos = new Map();
+
+export function addTodo(key, object) {
+    if (typeof(key) !== 'string') {
+        throw new TypeError('Key must be of type: String');
+    }
+
+    if (!key.trim()) {
+        throw new Error('Key cannot be empty');
+    }
+
+    if (object instanceof Todo === false) {
+        throw new TypeError(`${object} must be an instance of: Todo`);
+    }
+
+    activeTodos.set(key.trim(), structuredClone(object));
+}
