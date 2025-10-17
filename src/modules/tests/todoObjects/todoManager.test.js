1fc5108ce14047ded07852841c1ede195746efd5
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-17 13:10:56 -0400

    refactor: Export todoManager as factory

--- src/modules/tests/todoObjects/todoManager.test.js
+++ src/modules/tests/todoObjects/todoManager.test.js
@@ -1,71 +1,78 @@
-import { addTodo, getAllObjects, getTodoObject, deleteTodoObject } from "../../todoObjects/todoManager";
+import { createTodoManager } from "../../todoObjects/todoManager";
 import { Todo } from "../../todoObjects/createTodoObj";
 
-describe('addTodo', () => {
-    describe('when passed invalid parameters', () => {
-        describe('to key parameter', () => {
+describe('createTodoManager', () => {
+    let todoManager;
+    beforeEach(() => {
+        todoManager = createTodoManager();
+    })
+
+    describe('addTodo', () => {
+        describe('when passed invalid parameters', () => {
+            describe('to key parameter', () => {
+                const validObj = new Todo();
+                test.each([
+                    {description: 'number', input: 123},
+                    {description: 'array', input: []},
+                    {description: 'object', input: {}},
+                    {description: 'null', input: null},
+                    {description: 'undefined', input: undefined},
+                ])('throws TypeError when passed $description as key', ({description, input}) => {
+                    expect(() => todoManager.addTodo(input, validObj)).toThrow(TypeError);
+                });
+
+                test.each([
+                    {description: 'empty string', input: ''},
+                    {description: 'White space only', input: '     '},
+                    {description: 'String constructor', input: new String()},
+                ])('throws Error when passed $description as key', ({description, input}) => {
+                    expect(() => todoManager.addTodo(input, validObj)).toThrow(Error);
+                });
+            });
+
+            describe('to object parameter', () => {
+                const validKey = 'ValidKey';
+
+                test.each([
+                    {description: 'null', input: null},
+                    {description: 'undefined', input: undefined},
+                    {description: 'string', input: 'some string'},
+                    {description: 'number', input: 1}
+                ])('throws TypeError when passed $description', ({description, input}) => {
+                    expect(() => todoManager.addTodo(validKey, input)).toThrow(TypeError);
+                });
+            });
+        });
+
+        describe('when passed valid parameters', () => {
             const validObj = new Todo();
-            test.each([
-                {description: 'number', input: 123},
-                {description: 'array', input: []},
-                {description: 'object', input: {}},
-                {description: 'null', input: null},
-                {description: 'undefined', input: undefined},
-            ])('throws TypeError when passed $description as key', ({description, input}) => {
-                expect(() => addTodo(input, validObj)).toThrow(TypeError);
-            });
 
             test.each([
-                {description: 'empty string', input: ''},
-                {description: 'White space only', input: '     '},
-                {description: 'String constructor', input: new String()},
-            ])('throws Error when passed $description as key', ({description, input}) => {
-                expect(() => addTodo(input, validObj)).toThrow(Error);
+                {description: 'left padding', key: '   key'},
+                {description: 'right padding', key: 'key    '},
+                {description: 'left/right padding', key: '    key    '},
+            ])('strips $description whitespace from key', ({description, key}) => {
+                const result = addTodo(key, validObj);
+                const [resultKey] = result.keys();
+
+                expect(resultKey).toBe(key.trim());
             });
-        });
 
-        describe('to object parameter', () => {
-            const validKey = 'ValidKey';
+            test('Map contains expected key: value pairs & length upon succesful exection', () => {
+                let resultMap;
+                for (let i = 0; i < 5; ++i) {
+                    resultMap = todoManager.addTodo(`key${i}`, validObj);
+                }
 
-            test.each([
-                {description: 'null', input: null},
-                {description: 'undefined', input: undefined},
-                {description: 'string', input: 'some string'},
-                {description: 'number', input: 1}
-            ])('throws TypeError when passed $description', ({description, input}) => {
-                expect(() => addTodo(validKey, input)).toThrow(TypeError);
+                expect(resultMap.size).toBe(5);
+
+                let increment = 0;
+                for (const [key, value] of resultMap.entries()) {
+                    expect(key).toBe(`key${increment}`);
+                    expect(value).toBe(validObj);
+                    ++increment
+                }
             });
-        });
-    });
-
-    describe('when passed valid parameters', () => {
-        const validObj = new Todo();
-
-        test.each([
-            {description: 'left padding', key: '   key'},
-            {description: 'right padding', key: 'key    '},
-            {description: 'left/right padding', key: '    key    '},
-        ])('strips $description whitespace from key', ({description, key}) => {
-            const result = addTodo(key, validObj);
-            const [resultKey] = result.keys();
-
-            expect(resultKey).toBe(key.trim());
-        });
-
-        test('Map contains expected key: value pairs & length upon succesful exection', () => {
-            let resultMap;
-            for (let i = 0; i < 5; ++i) {
-                resultMap = addTodo(`key${i}`, validObj);
-            }
-
-            expect(resultMap.size).toHaveLength(5);
-
-            let increment = 0;
-            for (const [key, value] of resultMap.entries()) {
-                expect(key).toBe(`key${increment}`);
-                expect(value).toBe(validObj);
-                ++increment
-            }
         });
     });
 });
