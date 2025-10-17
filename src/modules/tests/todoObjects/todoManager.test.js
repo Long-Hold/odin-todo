ddbeb4316193dc38f7303846997662e8c9583851
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-17 09:23:01 -0400

    refactor: Group tests in new describe block

--- src/modules/tests/todoObjects/todoManager.test.js
+++ src/modules/tests/todoObjects/todoManager.test.js
@@ -4,22 +4,25 @@ class Todo {}
 
 describe('addTodo', () => {
     describe('when passed invalid parameters', () => {
-        test.each([
-            {description: 'number', input: 123},
-            {description: 'array', input: []},
-            {description: 'object', input: {}},
-            {description: 'null', input: null},
-            {description: 'undefined', input: undefined},
-        ])('throws TypeError when passed $description as key', ({description, input}) => {
-            expect(() => addTodo(input, new Todo)).toThrow(TypeError);
-        });
+        describe('to key parameter', () => {
+            const validObj = new Todo();
+            test.each([
+                {description: 'number', input: 123},
+                {description: 'array', input: []},
+                {description: 'object', input: {}},
+                {description: 'null', input: null},
+                {description: 'undefined', input: undefined},
+            ])('throws TypeError when passed $description as key', ({description, input}) => {
+                expect(() => addTodo(input, validObj)).toThrow(TypeError);
+            });
 
-        test.each([
-            {description: 'empty string', input: ''},
-            {description: 'White space only', input: '     '},
-            {description: 'String constructor', input: new String()},
-        ])('throws Error when passed $description as key', ({description, input}) => {
-            expect(() => addTodo(input, new Todo())).toThrow(Error);
+            test.each([
+                {description: 'empty string', input: ''},
+                {description: 'White space only', input: '     '},
+                {description: 'String constructor', input: new String()},
+            ])('throws Error when passed $description as key', ({description, input}) => {
+                expect(() => addTodo(input, validObj)).toThrow(Error);
+            });
         });
     });
 });
