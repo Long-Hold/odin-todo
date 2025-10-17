1b04ffe2a787f3027454548aa54ca63589d3610c
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-17 10:57:30 -0400

    feat: Add tests to verify object param error catching

--- src/modules/tests/todoObjects/todoManager.test.js
+++ src/modules/tests/todoObjects/todoManager.test.js
@@ -23,5 +23,18 @@ describe('addTodo', () => {
                 expect(() => addTodo(input, validObj)).toThrow(Error);
             });
         });
+
+        describe('to object parameter', () => {
+            const validKey = 'ValidKey';
+
+            test.each([
+                {description: 'null', input: null},
+                {description: 'undefined', input: undefined},
+                {description: 'string', input: 'some string'},
+                {description: 'number', input: 1}
+            ])('throws TypeError when passed $description', ({description, input}) => {
+                expect(() => addTodo(validKey, input)).toThrow(TypeError);
+            });
+        });
     });
 });
