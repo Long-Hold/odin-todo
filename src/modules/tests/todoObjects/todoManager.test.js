d680434f05c1c8c3a5376b0685d0f261c3b8fa36
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-17 09:14:54 -0400

    feat: Add tests to verify TypeError is thrown

--- src/modules/tests/todoObjects/todoManager.test.js
+++ src/modules/tests/todoObjects/todoManager.test.js
@@ -1,1 +1,17 @@
 import { addTodo, getAllObjects, getTodoObject, deleteTodoObject } from "../../todoObjects/todoManager";
+
+class Todo {}
+
+describe('addTodo', () => {
+    describe('when passed invalid parameters', () => {
+        test.each([
+            {description: 'number', input: 123},
+            {description: 'array', input: []},
+            {description: 'object', input: {}},
+            {description: 'null', input: null},
+            {description: 'undefined', input: undefined},
+        ])('throws TypeError when passed $description as key', ({description, input}) => {
+            expect(() => addTodo(input, new Todo)).toThrow(TypeError);
+        });
+    });
+});
