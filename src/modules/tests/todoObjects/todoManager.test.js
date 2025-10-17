8bcb839e999cb482b7f06bc19bc9469cc9914751
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-17 09:20:07 -0400

    feat: Add tests to verify Error is thrown

--- src/modules/tests/todoObjects/todoManager.test.js
+++ src/modules/tests/todoObjects/todoManager.test.js
@@ -13,5 +13,13 @@ describe('addTodo', () => {
         ])('throws TypeError when passed $description as key', ({description, input}) => {
             expect(() => addTodo(input, new Todo)).toThrow(TypeError);
         });
+
+        test.each([
+            {description: 'empty string', input: ''},
+            {description: 'White space only', input: '     '},
+            {description: 'String constructor', input: new String()},
+        ])('throws Error when passed $description as key', ({description, input}) => {
+            expect(() => addTodo(input, new Todo())).toThrow(Error);
+        });
     });
 });
