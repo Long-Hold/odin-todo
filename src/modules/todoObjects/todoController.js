08a9d823f3827dcf98159aea33631e6fb43414b3
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-20 14:20:59 -0400

    refactor: Import form node variable

--- src/modules/todoObjects/todoController.js
+++ src/modules/todoObjects/todoController.js
@@ -1,4 +1,4 @@
 import { createTodoObject } from "./createTodoObj";
 import { createTodoManager } from "./todoManager";
 
-import { processSubmit, resetAndCloseForm } from "../forms/formController";
+import { TODO_FORM, processSubmit, resetAndCloseForm } from "../forms/formController";
