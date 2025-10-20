58fb54e30c10a9095bb9bc5b6d29ccf819cb5f0d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-20 15:04:27 -0400

    feat: Add object and storage logic to todoController module

--- src/modules/todoObjects/todoController.js
+++ src/modules/todoObjects/todoController.js
@@ -2,3 +2,34 @@ import { createTodoObject } from "./createTodoObj";
 import { createTodoManager } from "./todoManager";
 
 import { TODO_FORM, processSubmit, resetAndCloseForm } from "../forms/formController";
+
+export const TODO_OBJECT_MANAGER = createTodoManager();
+
+export function initializeTodoController() {
+    initializeFormSubmissionListener();
+}
+
+function initializeFormSubmissionListener() {
+    TODO_FORM.addEventListener('submit', (event) => {
+        const formObject = processSubmit(event);
+        
+        if (formObject !== null) {
+            transactTodoObjectCreation(formObject);
+        }
+
+        resetAndCloseForm();
+    });
+}
+
+function transactTodoObjectCreation(formObject) {
+    const todoObject = createTodoObject(formObject);
+
+    try {
+        TODO_OBJECT_MANAGER.addTodo(todoObject.taskID, todoObject);
+    } catch(error) {
+        console.error(`${transactTodoObjectCreation.name} -> ${TODO_OBJECT_MANAGER.addTodo.name}: ${error}`);
+        return null;
+    }
+
+    return todoObject;
+}
