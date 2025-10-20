18f63c68dff7f591e9eb0e6ef484b3b1285740d8
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-20 18:50:29 -0400

    feat: Create state function to coordinate synchronization transactions

--- src/modules/state/stateController.js
+++ src/modules/state/stateController.js
@@ -1,4 +1,26 @@
 import { TODO_FORM, processSubmit, resetAndCloseForm } from "../forms/formController";
-import { TODO_OBJECT_MANAGER, transactTodoObjectCreation} from "../todoObjects/todoController";
+import { TODO_OBJECT_MANAGER, createAndSaveTodoObj} from "../todoObjects/todoController";
 import { renderSingleCard, renderAllCards } from "../ui/uiController";
 
+function newTodoFormSubmitListener() {
+    let formObject;
+
+    TODO_FORM.addEventListener('submit', (event) => {
+        event.preventDefault();
+        formObject = processSubmit(event);
+        resetAndCloseForm();
+    })
+
+    if (formObject === null) {
+        console.error('formObject returned with null. Aborting task creation.');
+        return null;
+    }
+
+    const newTodoObj = createAndSaveTodoObj(formObject);
+    if (newTodoObj === null) {
+        console.error(`${createAndSaveTodoObj.name} returned with null. Aborting task creation.`);
+        return null;
+    }
+
+    renderSingleCard(newTodoObj);
+}
