9c94576a27b9089b4cbf11a3f7131528fbeab949
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-20 18:58:35 -0400

    refactor: Move logic inside event listener callback

--- src/modules/state/stateController.js
+++ src/modules/state/stateController.js
@@ -3,24 +3,25 @@ import { TODO_OBJECT_MANAGER, createAndSaveTodoObj} from "../todoObjects/todoCon
 import { renderSingleCard, renderAllCards } from "../ui/uiController";
 
 function newTodoFormSubmitListener() {
-    let formObject;
-
     TODO_FORM.addEventListener('submit', (event) => {
-        event.preventDefault();
-        formObject = processSubmit(event);
+        const formObject = processSubmit(event);
+        if (formObject === null) {
+            console.error('formObject returned with null. Aborting task creation.');
+            return null;
+        }
+
+        const newTodoObj = createAndSaveTodoObj(formObject);
+        if (newTodoObj === null) {
+            console.error(`${createAndSaveTodoObj.name} returned with null. Aborting task creation.`);
+            return null;
+        }
+
+        renderSingleCard(newTodoObj);
+        
         resetAndCloseForm();
     })
+}
 
-    if (formObject === null) {
-        console.error('formObject returned with null. Aborting task creation.');
-        return null;
-    }
-
-    const newTodoObj = createAndSaveTodoObj(formObject);
-    if (newTodoObj === null) {
-        console.error(`${createAndSaveTodoObj.name} returned with null. Aborting task creation.`);
-        return null;
-    }
-
-    renderSingleCard(newTodoObj);
+export function initializeNewTodoState() {
+    newTodoFormSubmitListener();
 }
