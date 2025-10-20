6a67166ffb1974500e2edd29698237c1d2bdd1a1
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-20 18:02:13 -0400

    feat: Add function that calls respective cardCreator functions

--- src/modules/ui/uiController.js
+++ src/modules/ui/uiController.js
@@ -1,2 +1,39 @@
 import { createCardCreator } from "./createTodoCard";
 import { displayNewCardNode, clearDisplayBox } from "./renderCards";
+
+function constructTodoCardFromObject(todoCardObj) {
+    const cardCreator = createCardCreator();
+    for (const property in todoCardObj) {
+        switch (property) {
+            case ('taskID'):
+                cardCreator.setCardID(todoCardObj[property]);
+                break;
+            
+            case ('title'):
+                cardCreator.setTitle(todoCardObj[property]);
+                break;
+
+            case ('priority'):
+                cardCreator.setPriority(todoCardObj[property]);
+                break;
+            
+            case('projects'):
+                cardCreator.setProject(todoCardObj[property]);
+                break;
+            
+            case('deadline'):
+                cardCreator.setDeadline(new Date(todoCardObj[property]));
+                break;
+            
+            case('description'):
+                cardCreator.setDescription(todoCardObj[property]);
+                break;
+            
+            case('steps'):
+                cardCreator.setChecklistSteps(todoCardObj[property])
+                break;
+        }
+    }
+
+    return cardCreator.getTodoCard();
+}
