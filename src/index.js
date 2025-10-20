bc479436ea47e683dbf035324f78ab59bc021a2b
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-20 18:51:56 -0400

    refactor: Remove unused function and imports

--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
 import "./styles/styles.css";
-import { createCardCreator } from "./modules/ui/createTodoCard";
 import { initializeFormControl } from "./modules/forms/formController";
-import { TODO_OBJECT_MANAGER, initializeTodoController } from "./modules/todoObjects/todoController";
-
-function customizeTodoCard(todoCardObj) {
-    const cardCreator = createCardCreator();
-    for (const property in todoCardObj) {
-        switch (property) {
-            case ('taskID'):
-                cardCreator.setCardID(todoCardObj[property]);
-                break;
-            
-            case ('title'):
-                cardCreator.setTitle(todoCardObj[property]);
-                break;
-
-            case ('priority'):
-                cardCreator.setPriority(todoCardObj[property]);
-                break;
-            
-            case('projects'):
-                cardCreator.setProject(todoCardObj[property]);
-                break;
-            
-            case('deadline'):
-                cardCreator.setDeadline(new Date(todoCardObj[property]));
-                break;
-            
-            case('description'):
-                cardCreator.setDescription(todoCardObj[property]);
-                break;
-            
-            case('steps'):
-                cardCreator.setChecklistSteps(todoCardObj[property])
-                break;
-        }
-    }
-
-    return cardCreator.getTodoCard();
-}
+import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
 
 initializeFormControl();
 initializeTodoController();
