dd8c0e74afa0b2034cb8cb44cc52c79eb4c87fe0
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-21 14:33:31 -0400

    refactor: Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles/styles.css";
 import { initializeFormControl } from "./modules/forms/formController";
 import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
-import { initializeNewTodoState } from "./modules/state/stateController";
 
 initializeFormControl();
 
