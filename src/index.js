b473b501ae62fd2001b3cc57a520bdeb89a8a73c
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-21 17:09:05 -0400

    feat: Import method from state module to initialize object loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./styles/styles.css";
 import { initializeFormControl } from "./modules/forms/formController";
+import { initializeStorageAndUIStates } from "./modules/state/stateController";
 import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
 
+initializeStorageAndUIStates();
 initializeFormControl();
 
 window.todoMan = TODO_OBJECT_MANAGER;
