979838186fe7be87e83d3a558fe34b725eca63a9
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-21 16:52:14 -0400

    feat: Add logic to save new objects to local storage upon creation

--- src/modules/state/stateController.js
+++ src/modules/state/stateController.js
@@ -1,11 +1,20 @@
 import { TODO_OBJECT_MANAGER, createAndSaveTodoObj, storeTodoObj} from "../todoObjects/todoController";
 import { renderSingleCard, renderAllCards } from "../ui/uiController";
-import { getAllTodoObjects } from "../storage/localStorageManager";
+import { getAllTodoObjects, saveToLocalStorage } from "../storage/localStorageManager";
 
 export function handleNewTodo(formObject) {
     const newTodoObj = createAndSaveTodoObj(formObject);
     if (newTodoObj === null) {
         console.error(`${createAndSaveTodoObj.name} returned with null. Aborting task creation.`);
+        return null;
+    }
+
+    try {
+        saveToLocalStorage(newTodoObj.taskID, newTodoObj);
+    } catch (error) {
+        // If an error occurs while saving to localStorage, delete the object from the map
+        console.error(`An error occured saving task to localStorage: ${error}`);
+        TODO_OBJECT_MANAGER.deleteTodoObject(newTodoObj.taskID);
         return null;
     }
 
