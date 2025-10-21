55c2cba1c90a0073483382198ad9e6ca5209b068
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-21 15:58:32 -0400

    feat: Add storage module methods to save and retrieve objects from localStorage

---
+++ src/modules/storage/localStorageManager.js
@@ -0,0 +1,32 @@
+export function saveToLocalStorage(key, item) {
+  if (item instanceof Object === false) {
+    throw new TypeError(`Expect item to be instanceof Object. Received ${typeof(item)}`);
+  }
+
+  if (typeof(key) !== 'string') {
+    throw new TypeError(`Key must be string. Received ${typeof(key)}`);
+  }
+
+  const trimKey = key.trim();
+  if (!trimKey) {
+    throw new Error('Key cannot be a blank or whitespace-only string');
+  }
+
+  localStorage.setItem(trimKey, JSON.stringify(item));
+}
+
+export function getTodoObject(key) {
+  if (typeof(key) !== 'string') {
+    throw new TypeError(`Key must be string. Received ${typeof(key)}`);
+  }
+
+  const trimKey = key.trim();
+  if (!trimKey) {
+    throw new Error('Key cannot be a blank or whitespace-only string');
+  }
+
+  const todoJSON = localStorage.getItem(key);
+  const todoObj = JSON.parse(todoJSON);
+
+  return todoObj;
+}
