d052e1e616242fd30a9094f74f05a4eaeca1d05e
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-10-21 14:29:47 -0400

    feat: Remove unused imports and functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
 import { initializeNewTodoState } from "./modules/state/stateController";
 
 initializeFormControl();
-initializeNewTodoState();
 
 window.todoMan = TODO_OBJECT_MANAGER;
--- src/modules/state/stateController.js
+++ src/modules/state/stateController.js
@@ -1,30 +1,5 @@
-import { TODO_FORM, processSubmit, resetAndCloseForm } from "../forms/formController";
 import { TODO_OBJECT_MANAGER, createAndSaveTodoObj} from "../todoObjects/todoController";
 import { renderSingleCard, renderAllCards } from "../ui/uiController";
-
-function newTodoFormSubmitListener() {
-    TODO_FORM.addEventListener('submit', (event) => {
-        const formObject = processSubmit(event);
-        if (formObject === null) {
-            console.error('formObject returned with null. Aborting task creation.');
-            return null;
-        }
-
-        const newTodoObj = createAndSaveTodoObj(formObject);
-        if (newTodoObj === null) {
-            console.error(`${createAndSaveTodoObj.name} returned with null. Aborting task creation.`);
-            return null;
-        }
-
-        renderSingleCard(newTodoObj);
-        
-        resetAndCloseForm();
-    })
-}
-
-export function initializeNewTodoState() {
-    newTodoFormSubmitListener();
-}
 
 export function handleNewTodo(formObject) {
     const newTodoObj = createAndSaveTodoObj(formObject);
