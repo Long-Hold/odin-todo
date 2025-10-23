06157f735952ee76d6c807e6247bdfa500e55e89
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-23 11:11:59 -0400

    feat: Add initializer function to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles/styles.css";
 import { initializeFormControl } from "./modules/forms/formController";
-import { initializeStorageAndUIStates } from "./modules/state/stateController";
+import { initializeStateListeners, initializeStorageAndUIStates } from "./modules/state/stateController";
 import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
 
 initializeStorageAndUIStates();
+initializeStateListeners();
 initializeFormControl();
 
 window.todoMan = TODO_OBJECT_MANAGER;
