fc1e9c516a49ea890517409ecf05d213e65abd02
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-23 19:08:58 -0400

    feat: Add events in initializer function to show dialog

--- src/modules/projects/projectController.js
+++ src/modules/projects/projectController.js
@@ -1,1 +1,11 @@
 const NEW_PROJECT_BUTTON = document.getElementById('add-project-btn');
+const NEW_PROJECT_DIALOG = document.getElementById('new-project-dialog');
+const NEW_PROJECT_FORM = document.getElementById('new-project-form');
+
+export function initializeNewProjectListeners() {
+    showNewProjectForm();
+}
+
+function showNewProjectForm() {
+    NEW_PROJECT_BUTTON.addEventListener('click', NEW_PROJECT_DIALOG.show());
+}
