1032a151653b667b3b9ef4eae2bd842acd56dbfe
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-23 19:10:28 -0400

    feat: Import projectController and call its initializer function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import "./styles/styles.css";
 import { initializeFormControl } from "./modules/forms/formController";
 import { initializeStateListeners, initializeStorageAndUIStates } from "./modules/state/stateController";
 import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
+import { initializeNewProjectListeners } from "./modules/projects/projectController";
 
 initializeStorageAndUIStates();
 initializeStateListeners();
 initializeFormControl();
+initializeNewProjectListeners();
 
 window.todoMan = TODO_OBJECT_MANAGER;
