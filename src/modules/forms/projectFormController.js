5a32bc7e759086aedaa92211a6b7955c819be064
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-26 19:25:39 -0400

    feat: Target new project form and other related elements

---
+++ src/modules/forms/projectFormController.js
@@ -0,0 +1,3 @@
+const ADD_PROJECT_BTN = document.getElementById('add-project-btn');
+const NEW_PROJECT_DIALOG = document.getElementById('new-project-dialog');
+const NEW_PROJECT_FORM = document.getElementById('new-project-form');
