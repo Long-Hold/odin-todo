424a7af36350370a893d7ea2a34cea2c0cc1498a
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-26 15:45:19 -0400

    feat: Implement class blueprint for projects

---
+++ src/modules/projects/createProject.js
@@ -0,0 +1,25 @@
+export class Project {
+    constructor(projectName, linkedTasks = new Set()) {
+        this.projectName = projectName;
+        this.linkedTasks = structuredClone(linkedTasks);
+    }
+
+    linkTaskId(taskId) {
+        if (this.linkedTasks.has(taskId)) {
+            console.log('Task is already in this project category');
+            return;
+        }
+
+        this.linkedTasks.add(taskId);
+        return structuredClone(this.linkedTasks);
+    }
+
+    removeTask(taskId) {
+        this.linkedTasks.delete(taskId);
+        return structuredClone(this.linkedTasks);
+    }
+
+    getAllLinkedTasks() {
+        return Array.from(structuredClone(this.linkedTasks));
+    }
+}
