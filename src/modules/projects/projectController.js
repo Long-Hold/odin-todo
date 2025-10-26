b8af5ca544a4e3a26f235f264b39baa5edf68bf7
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-26 16:24:42 -0400

    feat: add new function to handle form submission events for projects

--- src/modules/projects/projectController.js
+++ src/modules/projects/projectController.js
@@ -1,3 +1,6 @@
+import { triggerCustomEvent } from "../events/eventProducer";
+import { EVENTS } from "../events/events";
+
 const NEW_PROJECT_BUTTON = document.getElementById('add-project-btn');
 const NEW_PROJECT_DIALOG = document.getElementById('new-project-dialog');
 const NEW_PROJECT_FORM = document.getElementById('new-project-form');
@@ -8,4 +11,18 @@ export function initializeNewProjectListeners() {
 
 function showNewProjectForm() {
     NEW_PROJECT_BUTTON.addEventListener('click', () => NEW_PROJECT_DIALOG.show());
+}
+
+function handleProjectFormSubmit() {
+    NEW_PROJECT_FORM.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        triggerCustomEvent(NEW_PROJECT_FORM, EVENTS.PROJECT_SUBMITTED, formData);
+        resetAndClose();
+    })
+}
+
+function resetAndClose() {
+    NEW_PROJECT_FORM.reset();
+    NEW_PROJECT_DIALOG.close();
 }
