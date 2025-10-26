f0d8e4f292b3babeeb2636ea5015835039a6beb4
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-26 19:29:06 -0400

    feat: Add function to toggle project dialog on or off

--- src/modules/forms/projectFormController.js
+++ src/modules/forms/projectFormController.js
@@ -1,3 +1,7 @@
 const ADD_PROJECT_BTN = document.getElementById('add-project-btn');
 const NEW_PROJECT_DIALOG = document.getElementById('new-project-dialog');
 const NEW_PROJECT_FORM = document.getElementById('new-project-form');
+
+function initializeAddProjectBtnListener() {
+    ADD_PROJECT_BTN.addEventListener('click', () => { NEW_PROJECT_DIALOG.show(); });
+}
