47934da5f5306c67551ee6a3c84b12c259019cb1
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-26 19:54:59 -0400

    feat: Add initializer function for project form control

--- src/modules/forms/projectFormController.js
+++ src/modules/forms/projectFormController.js
@@ -2,6 +2,11 @@ const ADD_PROJECT_BTN = document.getElementById('add-project-btn');
 const NEW_PROJECT_DIALOG = document.getElementById('new-project-dialog');
 const NEW_PROJECT_FORM = document.getElementById('new-project-form');
 
+export function initializeProjectFormControl() {
+    initializeAddProjectBtnListener();
+    initializeFormListeners();
+}
+
 function initializeAddProjectBtnListener() {
     ADD_PROJECT_BTN.addEventListener('click', () => { NEW_PROJECT_DIALOG.show(); });
 }
