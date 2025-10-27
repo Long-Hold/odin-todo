d8fe345903495a484b9536e29ba9ac44fa172873
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-27 18:49:42 -0400

    refactor: Remove unused module file

--- src/modules/projects/projectManager.js
+++
@@ -1,8 +0,0 @@
-import { createObjectManager } from "../objectManagers/createObjManager";
-
-export const {
-    addObject: addProject,
-    getAllObjects: getAllProjects,
-    getObject: getProject,
-    deleteObject: deleteProject,
-} = createObjectManager();
