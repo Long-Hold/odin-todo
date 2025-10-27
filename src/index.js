4a4be17d3950dd3d7d6fa955992f5cd9e3c5c473
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-27 16:28:17 -0400

    refactor: Update function name and import path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./styles/styles.css";
-import { initializeFormControl } from "./modules/forms/formController";
+import { initializeTodoFormControl } from "./modules/forms/todoFormController";
 import { initializeStateListeners, initializeStorageAndUIStates } from "./modules/state/stateController";
 import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
 import { initializeProjectFormControl } from "./modules/forms/projectFormController";
 
 initializeStorageAndUIStates();
 initializeStateListeners();
-initializeFormControl();
+initializeTodoFormControl();
 initializeProjectFormControl();
 
 window.todoMan = TODO_OBJECT_MANAGER;
