8c3d80d66d882027570a5355151a423f44d2a011
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-29 11:37:56 -0400

    feat: Set up test object for testing

--- src/modules/tests/projects/createProject.test.js
+++ src/modules/tests/projects/createProject.test.js
@@ -1,8 +1,12 @@
 import { Project } from "../../projects/createProject";
 
 describe('class Project', () => {
-    let myProjects;
+    let myProject;
     beforeEach(() => {
-        
+        const testIDs = ['1','2','3','4'];
+        const testSet = new Set(testIDs);
+        const projectName = 'Test Project';
+
+        myProject = new Project(projectName, testSet);
     });
 });
