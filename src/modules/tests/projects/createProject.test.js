d2752ee2f066080748cb30b682567f90aa027744
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-29 12:05:03 -0400

    feat: Add tests for invalid constructor paramters

--- src/modules/tests/projects/createProject.test.js
+++ src/modules/tests/projects/createProject.test.js
@@ -9,4 +9,29 @@ describe('class Project', () => {
 
         myProject = new Project(projectName, testSet);
     });
+
+    describe('when passed invalid paramters to constructor', () => {
+        describe('to projectName property', () => {
+            test.each([
+                {description: 'undefined', input: undefined},
+                {description: 'null', input: null},
+                {description: 'empty string', input: ''},
+                {description: 'whitespace only', input: '    '},
+            ])('throws Error when passed $description input', ({description, input}) => {
+                expect(() => new Project(input, new Set())).toThrow(Error);
+            });
+        });
+
+        describe('to linkedTasks paramter', () => {
+            test.each([
+                {description: 'array', input: []},
+                {description: 'object', input: {}},
+                {description: 'map', input: new Map()},
+                {description: 'string', input: 'Hello, world'},
+                {description: 'number', input: 123},
+            ])('throws TypeError for $description input', ({description, input}) => {
+                expect(() => new Project('My task!', input)).toThrow(TypeError);
+            });
+        });
+    });
 });
