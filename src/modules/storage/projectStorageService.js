555cb6ae65882f8f19c139d12e72619739645275
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-29 17:46:00 -0400

    refactor: Remove todo message

--- src/modules/storage/projectStorageService.js
+++ src/modules/storage/projectStorageService.js
@@ -5,6 +5,5 @@ export function saveProject(key, projectObj) {
     const jsonData = JSON.stringify(projectObj);
     const processedKey = normalizeKey(key);
     const storageKey = `${KEY_PREFIX}${processedKey}`;
-    // TODO: Save to local storage with custom key
     localStorage.setItem(storageKey, jsonData);
 }
