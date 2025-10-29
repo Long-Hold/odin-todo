bda377e30ef51fa99e570a9a67df1b96d567d0cb
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-10-29 15:43:07 -0400

    feat: Add boilerplate logic to handle new project event

--- src/modules/state/projectStateController.js
+++ src/modules/state/projectStateController.js
@@ -1,6 +1,14 @@
 import { Project } from "../projects/createProject";
 import { PROJECT_MANAGER } from "../projects/projectController";
+import { NEW_PROJECT_FORM } from "../forms/projectFormController";
+import { EVENTS } from "../events/events";
 
 export function initializeProjectState() {
-    
+    NEW_PROJECT_FORM.addEventListener(EVENTS.PROJECT_SUBMITTED, (event) => {
+        handleNewProject(event.detail.data)
+    });
+}
+
+function handleNewProject(formObject) {
+    console.log(formObject);
 }
