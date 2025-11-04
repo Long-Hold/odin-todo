50bfae673b70e25b206b1f2a5a24caaac3548704
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-11-04 11:59:59 -0500

    refactor: Remove unused imports and adjust spacing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import "./styles/styles.css";
+
 import { initializeTodoFormControl } from "./modules/forms/newTodoForm/todoFormController";
-import { initializeStateListeners, initializeStorageAndUIStates } from "./modules/state/stateController";
-import { TODO_OBJECT_MANAGER } from "./modules/todoObjects/todoController";
 import { initializeProjectFormControl } from "./modules/forms/newProjectForm/projectFormController";
+
 import { initializeTodoState } from "./modules/state/todos/todoStateController";
 import { initializeProjectState } from "./modules/state/projects/projectStateController";
 
 initializeTodoState();
 initializeProjectState();
+
 initializeTodoFormControl();
 initializeProjectFormControl();
-
-window.todoMan = TODO_OBJECT_MANAGER;
