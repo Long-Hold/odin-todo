f8d6c83b9658555631f58c2fb17108b3cf1e1861
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-11-05 14:03:23 -0500

    feat: Trigger event when a project tab is selected

--- src/modules/ui/tabs/projectTabController.js
+++ src/modules/ui/tabs/projectTabController.js
@@ -1,9 +1,13 @@
+import { triggerCustomEvent } from "../../events/eventProducer";
+import { EVENTS } from "../../events/events";
+
 const PROJECTS_TAB_CONTAINER = document.getElementById('projects-tab-container');
 
 export function initializeTabsListener() {
     PROJECTS_TAB_CONTAINER.addEventListener('click', (event) => {
-        if (event.target.classList.contains('project-btn') === false) {
-            console.log('ignore');
+        if (event.target.classList.contains('project-btn')) {
+            const projectName = event.target.dataset.projectName;
+            triggerCustomEvent(PROJECTS_TAB_CONTAINER, EVENTS.PROJECT_TABBED, projectName);
         }
     })
 }
