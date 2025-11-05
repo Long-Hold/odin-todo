1a4700bc3e03387e581cc7c73a28470d8fcd3368
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-11-05 13:29:17 -0500

    feat: Begin writing logic for webpage tab switcher

---
+++ src/modules/ui/tabs/projectTabController.js
@@ -0,0 +1,7 @@
+const PROJECTS_TAB_CONTAINER = document.getElementById('projects-tab-container');
+
+export function initializeTabsListener() {
+    PROJECTS_TAB_CONTAINER.addEventListener('click', (event) => {
+        //TODO
+    })
+}
