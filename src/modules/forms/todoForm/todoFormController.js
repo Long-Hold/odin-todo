0862367aa9317f5502686241039756f4ee2f2bc3
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-10 16:15:06 -0500

    feat: Add listener to show form dialog

--- src/modules/forms/todoForm/todoFormController.js
+++ src/modules/forms/todoForm/todoFormController.js
@@ -1,6 +1,10 @@
 const TODO_FORM = document.getElementById('todo-form');
+const ADD_TODO_BTN = document.getElementById('add-todo');
+const TODO_DIALOG = document.getElementById('todo-dialog');
 
 export function initializeTodoFormListeners() {
+    ADD_TODO_BTN.addEventListener('click', () => { TODO_DIALOG.show(); })
+
     TODO_FORM.addEventListener('submit', (event) => { 
         event.preventDefault();
         const formData = new FormData(event.target);
