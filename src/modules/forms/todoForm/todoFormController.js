ef7d59fa9f1aa97728431c2b45c228791a686274
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-10 17:19:48 -0500

    feat: Add event listeners to form control buttons

--- src/modules/forms/todoForm/todoFormController.js
+++ src/modules/forms/todoForm/todoFormController.js
@@ -1,9 +1,24 @@
 const TODO_FORM = document.getElementById('todo-form');
 const ADD_TODO_BTN = document.getElementById('add-todo');
 const TODO_DIALOG = document.getElementById('todo-dialog');
+const FORM_BUTTONS = document.getElementById('form-control-btns');
 
 export function initializeTodoFormListeners() {
     ADD_TODO_BTN.addEventListener('click', () => { TODO_DIALOG.show(); });
+
+    FORM_BUTTONS.addEventListener('click', (event) => {
+        const controlBtn = event.target;
+
+        if (controlBtn.dataset.action === 'reset') {
+            // TODO: Reset checklist steps
+            TODO_FORM.reset();
+        }
+
+        if (controlBtn.dataset.action === 'cancel') {
+            // TODO: Reset checklist steps
+            TODO_DIALOG.close();
+        }
+    })
 
     TODO_FORM.addEventListener('submit', (event) => { 
         event.preventDefault();
