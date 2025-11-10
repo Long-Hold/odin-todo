12f82490dbdbade03206baede49ea131bedb2886
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-10 15:45:30 -0500

    refactor: Delete deprecated imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
 import "./styles/styles.css";
-
-import { initializeTodoFormControl } from "./modules/forms/newTodoForm/todoFormController";
-import { initializeProjectFormControl } from "./modules/forms/newProjectForm/projectFormController";
-
-import { initializeTodoState } from "./modules/state/todos/todoStateController";
-import { initializeProjectState } from "./modules/state/projects/projectStateController";
-
-import { initializeTabsListener } from "./modules/ui/tabs/projectTabController";
-
-initializeTodoState();
-initializeProjectState();
-
-initializeTodoFormControl();
-initializeProjectFormControl();
-
-initializeTabsListener();
