5b061d451f31c1a38ec0cdc8788481fac8708e6e
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-12 17:28:46 -0500

    feat: Add module function that creates Todo obj from form data

--- src/modules/objects/todos/todoObjectController.js
+++ src/modules/objects/todos/todoObjectController.js
@@ -1,7 +1,14 @@
 import { Todo } from "./todoClass";
 
 export function createTodoFromFormData(formData) {
-    //TODO
+    const title = formData.get('title').trim();
+    const priority = formData.get('priority').trim();
+    const project = formData.get('projects').trim();
+    const deadline = formData.get('deadline').trim();
+    const description = formData.get('description').trim();
+    const checklist = Array.from(formData.querySelectorAll('checklistItem'));
+
+    return new Todo(title, priority, project, deadline, description, checklist);
 }
 
 export function createTodoFromLocalStorage(data) {
