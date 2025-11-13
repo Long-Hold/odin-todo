d80af4e1f8cd05765896d670ded7c3a513115682
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-13 14:22:03 -0500

    refactor: Remove composed function from todo manager

--- src/modules/objects/todos/todoObjectManager.js
+++ src/modules/objects/todos/todoObjectManager.js
@@ -13,13 +13,3 @@ const TODO_OBJECT_MANAGER = {
     getTodo,
     deleteTodo,
 };
-
-TODO_OBJECT_MANAGER.clearProperty = function (propertyName, key ) {
-    /**This function clears the specified property from propertyName, if it can be found,
-     * by settings it's value to null.
-     * 
-     * I retrieve the object by using a key from the Map that stores them to ensure
-     * only objects in the manager's set() are modified, as this manager exclusively works with
-     * todo objects and is not a general use object modifier method.
-     */
-}
