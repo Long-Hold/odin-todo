42b167b22a7947620652d6aff165dbf7f021cee0
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-12 22:19:35 -0500

    refactor: Assign null to empty entries and trim any checklist entries

--- src/modules/objects/todos/todoObjectController.js
+++ src/modules/objects/todos/todoObjectController.js
@@ -3,10 +3,22 @@ import { Todo } from "./todoClass";
 export function createTodoFromFormData(formData) {
     const title = formData.get('title').trim();
     const priority = formData.get('priority').trim();
-    const project = formData.get('projects').trim();
-    const deadline = formData.get('deadline').trim();
-    const description = formData.get('description').trim();
-    const checklist = Array.from(formData.querySelectorAll('checklistItem'));
+    const project = formData.get('projects')?.trim() || null;
+    const deadline = formData.get('deadline')?.trim() || null;
+    const description = formData.get('description')?.trim() || null;
+
+    /**If there are no checklist items, then checklist will remain as null.
+     * If there are any checklist items, then I iterate through each entry and trim the strings
+     * before assigning them to checklist.
+     * 
+     * This way the checklist property is assigned null on the object rather than as an empty
+     * array.
+     */
+    const checklistItems = Array.from(formData.getAll('checklistItem'));
+    let checklist = null;
+    if (checklistItems.length > 0) {
+        checklist = checklistItems.map(str => str.trim());
+    }
 
     return new Todo(title, priority, project, deadline, description, checklist);
 }
