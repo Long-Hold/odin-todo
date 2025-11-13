25e6692c7ed77162a0b3a12ed190d2dd6535eccf
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-13 00:58:00 -0500

    refactor: Remove error checking and structuredClone returns

--- src/modules/objectManagers/createObjManager.js
+++ src/modules/objectManagers/createObjManager.js
@@ -2,21 +2,13 @@ export function createObjectManager() {
     const storedObjects = new Map();
 
     function addObject(key, object) {
-        if (!key.trim()) {
-            throw new Error('Key cannot be empty');
-        }
-
-        if (object instanceof Object === false) {
-            throw new TypeError(`${object} must be an instance of: Object`);
-        }
-
         storedObjects.set(key.trim(), object);
 
-        return structuredClone(storedObjects);
+        return storedObjects;
     }
 
     function getAllObjects() {
-        return Array.from(storedObjects.values()).map(obj => structuredClone(obj));
+        return Array.from(storedObjects.values());
     }
 
     function getObject(key) {
@@ -26,7 +18,7 @@ export function createObjectManager() {
     function deleteObject(key) {
         storedObjects.delete(key);
 
-        return structuredClone(storedObjects);
+        return storedObjects;
     }
 
     return {addObject, getAllObjects, getObject, deleteObject}
