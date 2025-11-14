688870ca9e8180cef81cf67c63dfc7a27f166258
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-14 00:24:53 -0500

    feat: Import object state initializer function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { initializeTodoFormListeners } from "./modules/forms/todoForm/todoFormController";
+import { initializeTodoObjState } from "./modules/state/todoState/todoObjectState";
 import "./styles/styles.css";
 
 initializeTodoFormListeners();
+initializeTodoObjState();
