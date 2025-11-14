9aef7fab31883790fff0861c97879ec097b4a2a2
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-14 17:41:39 -0500

    refactor: Add prefix to todoIds

--- src/modules/objects/todos/todoClass.js
+++ src/modules/objects/todos/todoClass.js
@@ -1,6 +1,6 @@
 export class Todo {
     constructor(title, priority, project = null , deadline = null, description = null, checklist = null) {
-        this.todoId = crypto.randomUUID();
+        this.todoId = `todo_${crypto.randomUUID()}`;
         this.completed = false;
         this.title = title;
         this.priority = priority;
