8db1f9775fe1631e857dedc11e76e99c0ef80817
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-14 23:22:33 -0500

    feat: Add event listeners for project form

--- src/modules/forms/projectForm/projectFormController.js
+++ src/modules/forms/projectForm/projectFormController.js
@@ -1,3 +1,6 @@
+import { triggerCustomEvent } from "../../events/eventProducer";
+import { EVENTS } from "../../events/events";
+
 const ADD_PROJECT_BTN = document.getElementById('add-project-btn');
 const PROJECT_DIALOG = document.getElementById('project-dialog');
 
@@ -5,4 +8,19 @@ export const PROJECT_FORM = document.getElementById('project-form');
 
 export function initializeProjectFormListeners() {
     ADD_PROJECT_BTN.addEventListener('click', () => { PROJECT_DIALOG.show(); });
+    
+    PROJECT_FORM.addEventListener('click', (event) => {
+        if (event.target.dataset.action === 'cancel') { 
+            PROJECT_FORM.reset();
+            PROJECT_DIALOG.close();
+        }
+    })
+
+    PROJECT_FORM.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new formData(event.target);
+        triggerCustomEvent(PROJECT_FORM, EVENTS.PROJECT_FORM_SUBMITTED, formData);
+        PROJECT_FORM.reset();
+        PROJECT_DIALOG.close();
+    })
 }
