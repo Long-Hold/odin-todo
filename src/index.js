14138a9af4128e4152fc6eb86a592351750ac75d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-14 22:43:11 -0500

    feat: Export initializer function for project form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { initializeProjectFormListeners } from "./modules/forms/projectForm/projectFormController";
 import { initializeTodoFormListeners } from "./modules/forms/todoForm/todoFormController";
 import { initializeTodoObjState } from "./modules/state/todoState/todoObjectState";
 import "./styles/styles.css";
 
+initializeProjectFormListeners();
 initializeTodoFormListeners();
 initializeTodoObjState();
--- src/modules/forms/projectForm/projectFormController.js
+++ src/modules/forms/projectForm/projectFormController.js
@@ -1,2 +1,8 @@
 const ADD_PROJECT_BTN = document.getElementById('add-project-btn');
 const PROJECT_DIALOG = document.getElementById('project-dialog');
+
+export const PROJECT_FORM = document.getElementById('project-form');
+
+export function initializeProjectFormListeners() {
+    ADD_PROJECT_BTN.addEventListener('click', () => { PROJECT_DIALOG.show(); });
+}
