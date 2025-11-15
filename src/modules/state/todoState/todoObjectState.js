72b3b6bd900f5d45b07e675da69587fe49f814bd
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-14 21:57:53 -0500

    feat: Load localStorage objects to todo manager on page loag

--- src/modules/state/todoState/todoObjectState.js
+++ src/modules/state/todoState/todoObjectState.js
@@ -1,11 +1,25 @@
 import { EVENTS } from "../../events/events";
 import { TODO_FORM } from "../../forms/todoForm/todoFormController";
+import { TODO_ID_PREFIX } from "../../objects/todos/todoClass";
 import { createTodoFromFormData } from "../../objects/todos/todoObjectController";
 import { TODO_OBJECT_MANAGER } from "../../objects/todos/todoObjectManager";
+import { getAllPrefixedItems } from "../../storage/localStorageUtils";
 
 export function initializeTodoObjState() {
+    loadLocalStorageToManager();
     TODO_FORM.addEventListener(EVENTS.TODO_FORM_SUBMITTED, (event) => {
         const todoObject = createTodoFromFormData(event.detail.data);
         TODO_OBJECT_MANAGER.addTodo(todoObject.todoId, todoObject);
     })
+}
+
+function loadLocalStorageToManager() {
+    const todoObjectArray = getAllPrefixedItems(TODO_ID_PREFIX);
+    if (todoObjectArray.length === 0) {
+        console.log('No Todo objects to load from localStorage.');
+        return null;
+    }
+
+    todoObjectArray.forEach(todo => TODO_OBJECT_MANAGER.addTodo(todo.todoId, todo));
+    return TODO_OBJECT_MANAGER.getAllTodos();
 }
