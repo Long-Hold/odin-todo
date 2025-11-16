a67aeed567655340194e3ca24a0569641d5aa41b
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-16 18:18:45 -0500

    feat: Complete Project class constructor

--- src/modules/objects/projects/projectClass.js
+++ src/modules/objects/projects/projectClass.js
@@ -1,8 +1,8 @@
 export class Project {
     static ID_PREFIX = 'project_';
-    constructor(name, objectIds = null) {
+    constructor(name) {
         this.id = `${Project.ID_PREFIX}${crypto.randomUUID()}`;
         this.name = name;
-        //TODO: Linked Todos storage
+        this.objectIds = new Set();
     }
 }
