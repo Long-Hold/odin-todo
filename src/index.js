fc57e288fc60c596653936371e1bd7f6071a3934
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-17 09:01:15 -0500

    feat: Import project object state initializer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { initializeProjectFormListeners } from "./modules/forms/projectForm/projectFormController";
 import { initializeTodoFormListeners } from "./modules/forms/todoForm/todoFormController";
+import { initializeProjectObjectState } from "./modules/state/projectState/projectObjectState";
 import { initializeTodoObjState } from "./modules/state/todoState/todoObjectState";
 import "./styles/styles.css";
 
 initializeProjectFormListeners();
 initializeTodoFormListeners();
 initializeTodoObjState();
+initializeProjectObjectState();
