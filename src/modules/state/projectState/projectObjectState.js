7f79dff1efd768cdaa5f6f0e3ec7f829969522dd
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-17 09:09:17 -0500

    feat: Add method to load projects from localStorage into manager

--- src/modules/state/projectState/projectObjectState.js
+++ src/modules/state/projectState/projectObjectState.js
@@ -1,11 +1,24 @@
 import { EVENTS } from "../../events/events";
 import { PROJECT_FORM } from "../../forms/projectForm/projectFormController";
+import { Project } from "../../objects/projects/projectClass";
 import { createProjectFromFormData } from "../../objects/projects/projectObjectController";
 import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
+import { getAllPrefixedItems } from "../../storage/localStorageUtils";
 
 export function initializeProjectObjectState() {
+    loadProjectsFromLocalStorage();
     PROJECT_FORM.addEventListener(EVENTS.PROJECT_FORM_SUBMITTED, (event) => {
         const projectObj = createProjectFromFormData(event.detail.data);
         PROJECT_OBJECT_MANAGER.addProject(projectObj.id, projectObj);
     });
+}
+
+function loadProjectsFromLocalStorage() {
+    const projectsArray = getAllPrefixedItems(Project.ID_PREFIX);
+    projectsArray.forEach(jsonProj => {
+        const project = Project.fromJSON(jsonProj);
+        PROJECT_OBJECT_MANAGER.addProject(project.id, project);
+    });
+
+    return PROJECT_OBJECT_MANAGER.getAllProjects();
 }
