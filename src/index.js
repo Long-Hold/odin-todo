95f0d063d3c35d890a7ad12355e16cb09fceaee5
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-17 16:17:52 -0500

    feat: Add state manager for todo form to handle new project options

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { initializeProjectFormListeners } from "./modules/forms/projectForm/projectFormController";
 import { initializeTodoFormListeners } from "./modules/forms/todoForm/todoFormController";
 import { initializeProjectObjectState } from "./modules/state/projectState/projectObjectState";
+import { initializeTodoFormState } from "./modules/state/todoState/todoFormState";
 import { initializeTodoObjState } from "./modules/state/todoState/todoObjectState";
 import "./styles/styles.css";
 
+initializeTodoFormState();
 initializeProjectFormListeners();
 initializeTodoFormListeners();
 initializeTodoObjState();
---
+++ src/modules/state/todoState/todoFormState.js
@@ -0,0 +1,20 @@
+import { EVENTS } from "../../events/events";
+const PROJECTS_DROPDOWN_MENU = document.getElementById('projects-dropdown');
+
+export function initializeTodoFormState() {
+    document.addEventListener(EVENTS.PROJECT_CREATED, (event) => {
+        const projectsArray = event.detail.data;
+        updateProjectOptions(projectsArray);
+    })
+}
+
+function updateProjectOptions(projectsArray) {
+    projectsArray.forEach(project => {
+        const option = document.createElement('option');
+        option.value = project.id;
+        option.textContent = project.name;
+        PROJECTS_DROPDOWN_MENU.appendChild(option);
+    });
+
+    return PROJECTS_DROPDOWN_MENU;
+}
