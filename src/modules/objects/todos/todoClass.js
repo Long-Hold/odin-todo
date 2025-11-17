9e8cac1d86588858e426a188c49654423e8dedbc
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-17 09:28:27 -0500

    feat: Add static fromJSON method to Todo class

--- src/modules/objects/todos/todoClass.js
+++ src/modules/objects/todos/todoClass.js
@@ -1,6 +1,20 @@
 export class Todo {
     static ID_PREFIX = 'todo_';
 
+    static fromJSON(jsonObj) {
+        const todo = Object.create(Todo.prototype);
+        todo.todoId = jsonObj.todoId;
+        todo.completed = jsonObj.completed;
+        todo.title = jsonObj.title;
+        todo.priority = jsonObj.priority;
+        todo.project = jsonObj.project;
+        todo.deadline = jsonObj.deadline;
+        todo.description = jsonObj.description;
+        todo.checklist = jsonObj.checklist;
+
+        return todo;
+    }
+
     constructor(title, priority, project = null , deadline = null, description = null, checklist = null) {
         this.todoId = `${Todo.ID_PREFIX}${crypto.randomUUID()}`;
         this.completed = false;
