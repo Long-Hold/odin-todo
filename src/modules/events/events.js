fa16427664566c560d9f9610934c3b96b7c6a106
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-18 09:06:58 -0500

    feat: Add new event for todos that are part of a project

--- src/modules/events/events.js
+++ src/modules/events/events.js
@@ -2,4 +2,5 @@ export const EVENTS = {
     TODO_FORM_SUBMITTED: "todo:formSubmitted",
     PROJECT_FORM_SUBMITTED: "project:formSubmitted",
     PROJECT_CREATED: "project:created",
+    PROJECT_ASSIGNED: "project:assigned",
 }
