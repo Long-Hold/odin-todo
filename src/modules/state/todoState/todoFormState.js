ceb6fc62d00798342dc0f9585f4080aa756c8e08
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-17 23:13:22 -0500

    refactor: Move options rendering method to form UI module

--- src/modules/state/todoState/todoFormState.js
+++ src/modules/state/todoState/todoFormState.js
@@ -1,21 +1,9 @@
 import { EVENTS } from "../../events/events";
-const PROJECTS_DROPDOWN_MENU = document.getElementById('projects-dropdown');
+import { renderProjectOptions } from "../../forms/todoForm/todoFormUIController";
 
 export function initializeTodoFormState() {
     document.addEventListener(EVENTS.PROJECT_CREATED, (event) => {
         const projectsArray = event.detail.data;
-        updateProjectOptions(projectsArray);
-    })
-}
-
-// TODO: Move this function into the forms UI module for the todo form
-function updateProjectOptions(projectsArray) {
-    projectsArray.forEach(project => {
-        const option = document.createElement('option');
-        option.value = project.id;
-        option.textContent = project.name;
-        PROJECTS_DROPDOWN_MENU.appendChild(option);
+        renderProjectOptions(projectsArray);
     });
-
-    return PROJECTS_DROPDOWN_MENU;
 }
