41a3c3b67afb7ca9683b1dbba21e770df7fc9e3c
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-18 08:46:40 -0500

    refactor: Remove UI logic and related variables, imports

--- src/modules/forms/todoForm/todoFormController.js
+++ src/modules/forms/todoForm/todoFormController.js
@@ -1,30 +1,12 @@
 import { triggerCustomEvent } from "../../events/eventProducer";
 import { EVENTS } from "../../events/events";
-import { addStep, clearChecklistContainer, deleteStep } from "./checklistManager";
+import { initializeTodoFormUIButtons, resetForm } from "./todoFormUIController";
 
 export const TODO_FORM = document.getElementById('todo-form');
-const ADD_TODO_BTN = document.getElementById('add-todo');
 const TODO_DIALOG = document.getElementById('todo-dialog');
-const FORM_BUTTONS = document.getElementById('form-control-btns');
-
-const CHECKLIST_CONTAINER = document.getElementById('checklist-container');
-const CHECKLIST_INPUT_CONTAINER = document.getElementById('checklist-input-container');
 
 export function initializeTodoFormListeners() {
-    ADD_TODO_BTN.addEventListener('click', () => { TODO_DIALOG.show(); });
-
-    FORM_BUTTONS.addEventListener('click', (event) => {
-        const controlBtn = event.target.dataset.action;
-
-        if (controlBtn === 'reset') {
-            resetForm();
-        }
-
-        if (controlBtn === 'cancel') {
-            resetForm();
-            TODO_DIALOG.close();
-        }
-    });
+    initializeTodoFormUIButtons();
 
     TODO_FORM.addEventListener('submit', (event) => { 
         event.preventDefault();
@@ -33,25 +15,4 @@ export function initializeTodoFormListeners() {
         resetForm();
         TODO_DIALOG.close();
     });
-
-    CHECKLIST_CONTAINER.addEventListener('click', (event) => {
-        if (event.target.type !== 'button') { return; }
-
-        const clickedAction = event.target.dataset.action;
-
-        if (clickedAction === 'add-step') { 
-            const checklist_template = document.getElementById('checklist-template');
-            addStep(CHECKLIST_INPUT_CONTAINER, checklist_template);
-        }
-
-        if (clickedAction === 'delete') {
-            const stepDivContainer = event.target.parentElement;
-            deleteStep(stepDivContainer);
-        }
-    });
-}
-
-function resetForm() {
-    clearChecklistContainer(CHECKLIST_INPUT_CONTAINER);
-    TODO_FORM.reset();
 }
