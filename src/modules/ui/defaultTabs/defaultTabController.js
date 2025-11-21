492beb6648e9cd0ceeda3f4a65c1be4be3df2fab
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-21 16:16:47 -0500

    refactor: Simplify logic to a single event

--- src/modules/ui/defaultTabs/defaultTabController.js
+++ src/modules/ui/defaultTabs/defaultTabController.js
@@ -1,21 +1,13 @@
+import { triggerCustomEvent } from "../../events/eventProducer";
+import { EVENTS } from "../../events/events";
+
 const GENERAL_CATEGORIES = document.getElementById('general-categories');
 
 export function initializeDefaultTabListener() {
     GENERAL_CATEGORIES.addEventListener('click', (event) => {
-        if (event.target.tagName !== 'BUTTON') { return null; }
+        if (event.target.classList.contains('category-btn') === false) { return; }
 
         const filterDisplay = event.target.dataset.filterDisplay;
-
-        if (filterDisplay === 'all') { 
-            //todo
-        }
-
-        if (filterDisplay === 'today') {
-            //todo
-        }
-
-        if (filterDisplay === 'week') {
-            //todo
-        }
+        triggerCustomEvent(GENERAL_CATEGORIES, EVENTS.TAB_GENERAL_CLICKED, filterDisplay);
     });
 }
