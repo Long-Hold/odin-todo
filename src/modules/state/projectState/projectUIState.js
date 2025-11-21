4c58c14f2f11bd347b6a0e8f859fb01cedab96cf
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-21 13:14:52 -0500

    feat: Add new function that catches filter events, and passes filtered todo ids

--- src/modules/state/projectState/projectUIState.js
+++ src/modules/state/projectState/projectUIState.js
@@ -1,11 +1,23 @@
+import { triggerCustomEvent } from "../../events/eventProducer";
 import { EVENTS } from "../../events/events";
-import { initializeProjectTabListeners, renderProjectTabs } from "../../ui/projects/projectsTabHandler";
+import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
+import { initializeProjectTabListeners, PROJECTS_LIST, renderProjectTabs } from "../../ui/projects/projectsTabHandler";
 
 export function initializeProjectUIState() {
     listenForNewProjects();
     initializeProjectTabListeners();
+    listenForProjectTabFiltering();
 }
 
 function listenForNewProjects() {
     document.addEventListener(EVENTS.PROJECT_CREATED, (event) => { renderProjectTabs(event.detail.data); });
+}
+
+function listenForProjectTabFiltering() {
+    PROJECTS_LIST.addEventListener(EVENTS.PROJECT_TAB_CLICKED, (event) => {
+        const projectId = event.detail.data;
+        const projectObject = PROJECT_OBJECT_MANAGER.getProject(projectId);
+        const todoIdsArray = Array.from(projectObject.linkedIds);
+        triggerCustomEvent(document, EVENTS.TODO_FILTER_REQUESTED, todoIdsArray);
+    });
 }
