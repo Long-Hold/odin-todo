692ce083dd58d45e905d824a9225d475c12f8849
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-21 15:30:05 -0500

    feat: Add basic filtering logic

--- src/modules/state/todoState/todoUIState.js
+++ src/modules/state/todoState/todoUIState.js
@@ -1,13 +1,29 @@
 import { EVENTS } from "../../events/events";
+import { TODO_OBJECT_MANAGER } from "../../objects/todos/todoObjectManager";
 import { renderTodoCards } from "../../ui/todos/renderTodoCards";
 
 export function initializeTodoUIState() {
     listenForNewTodos();
+    listenForTodoFilterEvents();
 }
 
 function listenForNewTodos() {
     document.addEventListener(EVENTS.TODO_CREATED, (event) => {
         const todoObjArray = event.detail.data;
         renderTodoCards(todoObjArray);
+    });
+}
+
+function listenForTodoFilterEvents() {
+    document.addEventListener(EVENTS.TODO_FILTER_REQUESTED, (event) => {
+        const todoIdsArray = event.detail.data;
+        const todoObjectsArray = [];
+
+        todoIdsArray.forEach((id) => {
+            const todoObject = TODO_OBJECT_MANAGER.getTodo(id);
+            todoObjectsArray.push(todoObject);
+        });
+
+        renderTodoCards(todoObjectsArray);
     });
 }
