e2f4e7e6d8fbc834ed6375d3ec376767a7780ee1
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-21 16:39:27 -0500

    feat: Initialize default tabs and listen for tab filtering event

--- src/modules/state/todoState/todoUIState.js
+++ src/modules/state/todoState/todoUIState.js
@@ -1,11 +1,13 @@
 import { EVENTS } from "../../events/events";
 import { TODO_OBJECT_MANAGER } from "../../objects/todos/todoObjectManager";
+import { GENERAL_CATEGORIES, initializeDefaultTabListener } from "../../ui/defaultTabs/defaultTabController";
 import { renderTodoCards } from "../../ui/todos/renderTodoCards";
-import { enrichTodos } from "./todoStateUtils";
+import { enrichTodos, filterTodosByDate } from "./todoStateUtils";
 
 export function initializeTodoUIState() {
     listenForNewTodos();
     listenForTodoFilterEvents();
+    initializeDefaultTabListener();
 }
 
 function listenForNewTodos() {
@@ -21,6 +23,13 @@ function listenForTodoFilterEvents() {
         const rawTodos = todoIdsArray.map(id => TODO_OBJECT_MANAGER.getTodo(id));
         const enrichedTodos = enrichTodos(rawTodos);
 
+        renderTodoCards(enrichedTodos);
+    });
+
+    GENERAL_CATEGORIES.addEventListener(EVENTS.TAB_GENERAL_CLICKED, (event) => {
+        const dateString = event.detail.data;
+        const filteredRawTodos = filterTodosByDate(TODO_OBJECT_MANAGER.getAllTodos(), dateString);
+        const enrichedTodos = enrichTodos(filteredRawTodos);
         renderTodoCards(enrichedTodos);
     });
 }
