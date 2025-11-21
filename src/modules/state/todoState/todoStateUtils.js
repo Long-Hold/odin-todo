bfd7caa1ca81d848d89a697224dbb9d277a531fb
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-21 16:24:45 -0500

    feat: Add new util function to filter objects by date

--- src/modules/state/todoState/todoStateUtils.js
+++ src/modules/state/todoState/todoStateUtils.js
@@ -1,7 +1,8 @@
 import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
-
+import { isToday, isThisWeek } from "date-fns";
 /**
  * Enriches todo objects by replacing project ID with project name for display.
+ * 
  * Preserves original project ID as projectId property.
  * 
  * @param {Array} todosArray - Array of raw todo objects
@@ -14,4 +15,32 @@ export function enrichTodos(todosArray) {
         project: todo.project ? PROJECT_OBJECT_MANAGER.getProject(todo.project)?.name : null,
         projectId: todo.project,
     }));
+}
+
+
+/**
+ * Filters todo objects by comparing their deadline property with the specified date.
+ * 
+ * Ignores objects that do not have a value for the deadline property.
+ * 
+ * @param {Array} todosArray - Array of row todo objects
+ * @param {String} filterType - String representing the date range to filter objects by
+ * @returns {Array} Array of filtered objects, or the original Array if invalid filterType passed
+ */
+export function filterTodosByDate(todosArray, filterType) {
+    if (filterType === 'all') { return todosArray; }
+
+    if (filterType === 'today') {
+        return todosArray.filter((todo) => {
+            todo.deadline && isToday(new Date(todo.deadline));
+        });
+    }
+
+    if (filterType === 'week') {
+        return todosArray.filter((todo) => {
+            todo.deadline && isThisWeek(new Date(todo.deadline));
+        })
+    }
+
+    return todosArray;
 }
