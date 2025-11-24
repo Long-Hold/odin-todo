347d60998f0959c4221e3bd97f2c55b544e79f7a
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-24 17:04:53 -0500

    feat: Add filter tracking logic

---
+++ src/modules/state/filterState/filterStateController.js
@@ -0,0 +1,36 @@
+const GENERAL_TABS = document.getElementById('general-categories');
+const PROJECT_TABS = document.getElementById('projects-list');
+
+export const filterState = {
+    type: 'general',
+    display: 'all',
+}
+
+export function initializeFilterTabListeners() {
+    GENERAL_TABS.addEventListener('click', (event) => {
+        if (event.target.classList.contains('category-btn') === false) { return; }
+
+        const filterType = event.target.dataset.filterType;
+        const filterDisplay = event.target.dataset.filterDisplay;
+
+        updateFilterState(filterType, filterDisplay);
+    });
+
+    PROJECT_TABS.addEventListener('click', (event) => {
+        if (event.target.dataset.action !== 'filter') { return; }
+
+        const filterType = event.target.dataset.filterType;
+        const projectId = event.target.dataset.projectId;
+
+        updateFilterState(filterType, projectId);
+    });
+}
+
+/**Updates the filterState to track the last clicked filter button.
+ * 
+ * Emits an event for the render states to listen for to display the relevant todo objects.
+ */
+function updateFilterState(type, display) {
+    filterState.type = type;
+    filterState.display = display;
+}
