b2cfdbb1a03da97ce63805bae72870b5b03d8bc4
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-24 20:12:49 -0500

    refactor: Remove unused event aliases

--- src/modules/events/events.js
+++ src/modules/events/events.js
@@ -19,29 +19,12 @@ export const EVENTS = {
      * This event passes the Todo Object Id.
      */
     TODO_DELETED: "todo:deleted",
-
-    /**This event signals that specific Todo objects are being requested by
-     * a filter button, either a project tab button or the default filter buttons.
-     * 
-     * The data carried by this event carries an array of todo object id's that can be used
-     * to retrieve the relevant objects from the todo object manager for rendering to the UI
-     */
-    TODO_FILTER_REQUESTED: "todo:filterRequested",
     
     PROJECT_FORM_SUBMITTED: "project:formSubmitted",
     PROJECT_CREATED: "project:created",
     PROJECT_ASSIGNED: "project:assigned",
     PROJECT_DELETE_REQUESTED: "project:deleteRequested",
     PROJECT_DELETED: "project:deleted",
-
-    /**Signals that a user wants to filter the Todo cards
-     * displayed on screen by project. Only todo's with the respective
-     * tab that was clicked will be displayed when this event is emitted.
-     */
-    PROJECT_TAB_CLICKED: "project:tabClicked",
-
-    /**Signals that one of the general-use filter tab buttons have been clicked. */
-    TAB_GENERAL_CLICKED: "tab:generalClicked",
 
     /**Signals that a filter tab has been selected.
      * 
