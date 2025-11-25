a037d7d0e795c7f1a31656b0f6e451682c259c72
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-24 19:16:36 -0500

    feat: Set up new filtering listener in todoUIState

--- src/modules/state/todoState/todoUIState.js
+++ src/modules/state/todoState/todoUIState.js
@@ -1,8 +1,10 @@
 import { EVENTS } from "../../events/events";
+import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
 import { TODO_OBJECT_MANAGER } from "../../objects/todos/todoObjectManager";
 import { GENERAL_CATEGORIES, initializeDefaultTabListener } from "../../ui/defaultTabs/defaultTabController";
 import { renderTodoCards } from "../../ui/todos/renderTodoCards";
 import { initializeTodoCardListeners } from "../../ui/todos/todoCardController";
+import { getFilterState } from "../filterState/filterStateController";
 import { enrichTodos, filterTodosByDate } from "./todoStateUtils";
 
 export function initializeTodoUIState() {
@@ -10,6 +12,29 @@ export function initializeTodoUIState() {
     listenForTodoFilterEvents();
     initializeDefaultTabListener();
     initializeTodoCardListeners();
+    listenForFilterEvent();
+}
+
+/**Renders Todo Objects to the display depending on the last filter condition.
+ * 
+ * Retrieves the last filter state from getFilterState(), and then parses the Project and Todo object managers
+ * depending on the type of filter, and the display condition.
+ */
+export function renderAllTodos() {
+    const {type, display} = getFilterState();
+    if (type === 'general') {
+        const rawTodos = filterTodosByDate(TODO_OBJECT_MANAGER.getAllTodos(), display);
+        const enrichedTodos = enrichTodos(rawTodos);
+        renderTodoCards(enrichedTodos);
+    }
+
+    if (type === 'project') {
+        const projectObject = PROJECT_OBJECT_MANAGER.getProject(display);
+        const todoIdsArray = Array.from(projectObject.linkedIds);
+        const rawTodos = todoIdsArray.map(id => TODO_OBJECT_MANAGER.getTodo(id));
+        const enrichedTodos = enrichTodos(rawTodos);
+        renderTodoCards(enrichedTodos);
+    }
 }
 
 function listenForNewTodos() {
@@ -37,7 +62,7 @@ function listenForTodoFilterEvents() {
 }
 
 function listenForFilterEvent() {
-    document.addEventListener(EVENTS.FILTER_CHANGED, (event) => {
-        const {type, display} = event.detail.data;
-    })
+    document.addEventListener(EVENTS.FILTER_CHANGED, () => {
+        renderAllTodos();
+    });
 }
