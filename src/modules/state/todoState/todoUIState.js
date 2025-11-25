d94f5d34745baaa787cb54808379d5b807e64cee
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-24 19:25:16 -0500

    refactor: Remove unused module logic and related imports

--- src/modules/state/todoState/todoUIState.js
+++ src/modules/state/todoState/todoUIState.js
@@ -1,7 +1,6 @@
 import { EVENTS } from "../../events/events";
 import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
 import { TODO_OBJECT_MANAGER } from "../../objects/todos/todoObjectManager";
-import { GENERAL_CATEGORIES, initializeDefaultTabListener } from "../../ui/defaultTabs/defaultTabController";
 import { renderTodoCards } from "../../ui/todos/renderTodoCards";
 import { initializeTodoCardListeners } from "../../ui/todos/todoCardController";
 import { getFilterState } from "../filterState/filterStateController";
@@ -9,8 +8,6 @@ import { enrichTodos, filterTodosByDate } from "./todoStateUtils";
 
 export function initializeTodoUIState() {
     listenForNewTodos();
-    // listenForTodoFilterEvents();
-    // initializeDefaultTabListener();
     initializeTodoCardListeners();
     listenForFilterEvent();
 }
@@ -40,23 +37,6 @@ export function renderAllTodos() {
 function listenForNewTodos() {
     document.addEventListener(EVENTS.TODO_CREATED, (event) => {
         renderAllTodos();
-    });
-}
-
-function listenForTodoFilterEvents() {
-    document.addEventListener(EVENTS.TODO_FILTER_REQUESTED, (event) => {
-        const todoIdsArray = event.detail.data;
-        const rawTodos = todoIdsArray.map(id => TODO_OBJECT_MANAGER.getTodo(id));
-        const enrichedTodos = enrichTodos(rawTodos);
-
-        renderTodoCards(enrichedTodos);
-    });
-
-    GENERAL_CATEGORIES.addEventListener(EVENTS.TAB_GENERAL_CLICKED, (event) => {
-        const dateString = event.detail.data;
-        const filteredRawTodos = filterTodosByDate(TODO_OBJECT_MANAGER.getAllTodos(), dateString);
-        const enrichedTodos = enrichTodos(filteredRawTodos);
-        renderTodoCards(enrichedTodos);
     });
 }
 
