77e007af7d8587aca14e1c3b5957a5c9d650c783
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-25 16:30:24 -0500

    refactor: Remove unused todos and imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { initializeTodoFormListeners } from "./modules/forms/todoForm/todoFormCo
 import { broadcastProjects } from "./modules/state/projectState/projectObjectState";
 
 import { initializeProjectStates } from "./modules/state/projectState/projectStateController";
-import { broadcastTodos } from "./modules/state/todoState/todoObjectState";
 import { initializeTodoStates } from "./modules/state/todoState/todoStateController";
 
 import { initializeFilterTabListeners } from "./modules/state/filterState/filterStateController";
@@ -22,4 +21,3 @@ initializeFilterTabListeners();
  * I manually broadcast their data out for the listeners of respective objects to catch
  */
 broadcastProjects();
-broadcastTodos();
