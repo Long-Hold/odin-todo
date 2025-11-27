5b802340f1514501319cf4cc626156f0472ec1f3
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-11-26 21:24:56 -0500

    feat: Add prepopulation logic for todo edit events

--- src/modules/forms/todoForm/todoFormUIController.js
+++ src/modules/forms/todoForm/todoFormUIController.js
@@ -1,4 +1,6 @@
+import { EVENTS } from "../../events/events";
 import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
+import { TODO_OBJECT_MANAGER } from "../../objects/todos/todoObjectManager";
 import { addSelectOption, clearParentContainer } from "../formUIUtils";
 import { addStep, clearChecklistContainer, deleteStep } from "./checklistManager";
 
@@ -10,9 +12,11 @@ const FORM_BUTTONS = document.getElementById('form-control-btns');
 
 const CHECKLIST_CONTAINER = document.getElementById('checklist-container');
 const CHECKLIST_INPUT_CONTAINER = document.getElementById('checklist-input-container');
+const CHECKLIST_TEMPLATE = document.getElementById('checklist-template');
 
 export function initializeTodoFormUIButtons() {
-    ADD_TODO_BTN.addEventListener('click', () => { 
+    ADD_TODO_BTN.addEventListener('click', () => {
+        resetForm(); 
         renderProjectOptions();
         TODO_DIALOG.show(); 
     });
@@ -45,6 +49,16 @@ export function initializeTodoFormUIButtons() {
             deleteStep(stepDivContainer);
         }
     });
+
+    document.addEventListener(EVENTS.TODO_EDIT_REQUESTED, (event) => {
+        const todoId = event.detail.data;
+        const todoObj = TODO_OBJECT_MANAGER.getTodo(todoId);
+
+        resetForm();
+        renderProjectOptions();
+        populateFormForEdit(todoObj);
+        TODO_DIALOG.show();
+    })
 }
 
 export function resetForm() {
@@ -63,4 +77,29 @@ function renderProjectOptions() {
     projectsArray.forEach(project => addSelectOption(selectElement, project.id, project.name));
 
     return selectElement;
+}
+
+function populateFormForEdit(todo) {
+    console.log(todo);
+    // Populate hidden fields for edit mode
+    TODO_FORM.querySelector('#todo-id').value = todo.id;
+    TODO_FORM.querySelector('#created-at').value = todo.createdAt;
+
+    // The rest are visible fields
+
+    // Mandatory fields that are expected to always have a value
+    TODO_FORM.querySelector('#title').value = todo.title;
+    TODO_FORM.querySelector('#priority').value = todo.priority;
+
+    //Optional field population, which could potentially be blank
+    TODO_FORM.querySelector('#projects-dropdown').value = todo.project || '';
+    TODO_FORM.querySelector('#deadline').value = todo.deadline?.substring(0, 10) || '';
+    TODO_FORM.querySelector('#description').value = todo.description || '';
+
+    if (todo.checklist) {
+        for (const value of todo.checklist.values()) {
+            const step = addStep(CHECKLIST_INPUT_CONTAINER, CHECKLIST_TEMPLATE);
+            step.querySelector('input').value = value;
+        }
+    }
 }
