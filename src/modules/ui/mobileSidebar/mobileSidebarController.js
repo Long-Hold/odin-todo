10fecb3276efd8c204ef80f5ecd3228c0f691854
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-06 15:30:29 -0500

    feat: Add listener to close sidebar

--- src/modules/ui/mobileSidebar/mobileSidebarController.js
+++ src/modules/ui/mobileSidebar/mobileSidebarController.js
@@ -1,9 +1,20 @@
 const burgerButton = document.querySelector('.dropdown-btn-container');
 const sideBar = document.querySelector('.side-bar');
+const closeButton = document.querySelector('.close-btn');
 
 export function initializeMobileMenuListeners() {
     burgerButton.addEventListener('click', () => {
         changeSidebarVisibility();
+    });
+
+    sideBar.addEventListener('click', (event) => {
+        const closeBtn = event.target.closest('button');
+        
+        if (!closeBtn) { return; }
+
+        if (closeBtn.classList.contains('close-btn')) {
+            sideBar.classList.remove('open');
+        }
     })
 }
 
