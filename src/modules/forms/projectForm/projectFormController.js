582bf0d1325b1fb56402bda4c8896de15a2fe9bb
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-09 19:52:38 -0500

    refactor: Use showModal

--- src/modules/forms/projectForm/projectFormController.js
+++ src/modules/forms/projectForm/projectFormController.js
@@ -7,7 +7,7 @@ const PROJECT_DIALOG = document.getElementById('project-dialog');
 export const PROJECT_FORM = document.getElementById('project-form');
 
 export function initializeProjectFormListeners() {
-    ADD_PROJECT_BTN.addEventListener('click', () => { PROJECT_DIALOG.show(); });
+    ADD_PROJECT_BTN.addEventListener('click', () => { PROJECT_DIALOG.showModal(); });
     
     PROJECT_FORM.addEventListener('click', (event) => {
         if (event.target.dataset.action === 'cancel') { 
