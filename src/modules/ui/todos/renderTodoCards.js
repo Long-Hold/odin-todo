a876a79bb48658ae955c415d75c838d87a1c0a87
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-16 10:28:51 -0500

    refactor: Use new createCard function in render function

--- src/modules/ui/todos/renderTodoCards.js
+++ src/modules/ui/todos/renderTodoCards.js
@@ -9,44 +9,7 @@ export function renderTodoCards(todoObjectArray) {
 
     const sortedTodoArray = [...todoObjectArray].sort((a, b) => a.createdAt - b.createdAt);
     sortedTodoArray.forEach((todo) => {
-        const todoTemplateClone = TODO_CARD_TEMPLATE.content.cloneNode(true);
-
-        const article = todoTemplateClone.querySelector('.todo-card');
-        article.dataset.todoId = todo.id;
-
-        const statusButton = todoTemplateClone.querySelector('.todo-status');
-        if (todo.completed) {
-            statusButton.querySelector('use').setAttribute('href', '#circle-filled');
-        }
-
-        const title = todoTemplateClone.querySelector('.todo-title');
-        title.textContent = todo.title;
-
-        const priority = todoTemplateClone.querySelector('.todo-priority');
-        priority.textContent = todo.priority;
-        createPriorityColorBorder(priority, todo.priority);
-
-        const project = todoTemplateClone.querySelector('.todo-project');
-        project.textContent = todo.project || '';
-
-        const deadline = todoTemplateClone.querySelector('time');
-        if (todo.deadline === null) { deadline.textContent = ''; }
-        else if (todo.deadline) {
-            deadline.setAttribute('datetime', todo.deadline);
-            deadline.textContent = format(new Date(todo.deadline), 'MMM dd, yyyy');
-        }
-
-        const description = todoTemplateClone.querySelector('.todo-description');
-        description.textContent = todo.description;
-        
-        if (todo.checklist) {
-            const checklist = todoTemplateClone.querySelector('.todo-checklist');
-            todo.checklist.forEach((text, id) => {
-                const checklistElement = createChecklistItem(id, text);
-                checklist.appendChild(checklistElement);
-            });
-        }
-
+        const todoTemplateClone = createCard(todo);
         TODO_CARD_DISPLAY.appendChild(todoTemplateClone);
     });
 }
