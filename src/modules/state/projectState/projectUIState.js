63cac1a8d9b5f188cf3a7feb1ec4de735cf9ff68
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-17 11:22:41 -0500

    feat: Call updateCounter function when todo linked or unlinked

--- src/modules/state/projectState/projectUIState.js
+++ src/modules/state/projectState/projectUIState.js
@@ -1,6 +1,6 @@
 import { EVENTS } from "../../events/events";
 import { PROJECT_OBJECT_MANAGER } from "../../objects/projects/projectObjectManager";
-import { appendNewProject, initializeProjectTabListeners, renderProjectTabs } from "../../ui/projects/projectsTabHandler";
+import { appendNewProject, initializeProjectTabListeners, renderProjectTabs, updateTodoCounter } from "../../ui/projects/projectsTabHandler";
 
 export function initializeProjectUIState() {
     listenForDisplayUpdates();
@@ -15,5 +15,10 @@ function listenForDisplayUpdates() {
     document.addEventListener(EVENTS.PROJECT_CREATED, (event) => {
         const projectObj = event.detail.data;
         appendNewProject(projectObj);
+    });
+
+    document.addEventListener(EVENTS.PROJECT_SET_MUTATED, (event) => {
+        const { projectId, setLength } = event.detail.data;
+        updateTodoCounter(projectId, setLength);
     });
 }
