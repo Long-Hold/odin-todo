2258c58c3064257533eadc5a78f79ff140ab6733
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-17 12:17:05 -0500

    feat: Create new function to control general category counter

---
+++ src/modules/ui/generalTabs/generalTabsController.js
@@ -0,0 +1,18 @@
+const GENERAL_TABS = document.getElementById('general-categories');
+
+/**
+ * Updates the span element that shows how many todos are in that 
+ * particular general category.
+ * 
+ * @param {string} filterType - Which general tab to query for.
+ * @param {int} todoCount - The amount of todo's that are part of this category
+
+ */
+export function updateTodoCounter(filterType, todoCount) {
+    const selector = `[data-filter-type="${filterType}"]`;
+    const generalTab = GENERAL_TABS.querySelector(selector);
+    if (!generalTab) { return; }
+
+    const todoCounter = generalTab.querySelector('.todo-counter');
+    todoCounter.textContent = todoCount;
+}
