84c026708d72f4a101d6ca0a266579e10b93d7e0
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-12-17 18:04:04 -0500

    refactor: Change variable name and update docstring

--- src/modules/ui/generalTabs/generalTabsController.js
+++ src/modules/ui/generalTabs/generalTabsController.js
@@ -4,11 +4,11 @@ const GENERAL_TABS = document.getElementById('general-categories');
  * Updates the span element that shows how many todos are in that 
  * particular general category.
  * 
- * @param {string} filterType - Which general tab to query for.
+ * @param {string} filterDisplay - Which general tab to query for.
  * @param {int} todoCount - The amount of todo's that are part of this category
  */
-export function updateTodoCounter(filterType, todoCount) {
-    const selector = `[data-filter-type="${filterType}"]`;
+export function updateTodoCounter(filterDisplay, todoCount) {
+    const selector = `[data-filter-display="${filterDisplay}"]`;
     const generalTab = GENERAL_TABS.querySelector(selector);
     if (!generalTab) { return; }
 
